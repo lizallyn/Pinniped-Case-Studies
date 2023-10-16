40fe185b4483e4816d90cbe957e5673d5e1e33bd
Liz Allyn <liz.allyn@allyn.org>
2023-10-16 15:19:48 -0700

    switched back to public for now

--- Functions/Sockeye arrival function creation.R
+++ Functions/Sockeye arrival function creation.R
@@ -10,9 +10,9 @@ library(ggplot2)
 library(PNWColors)
 
 ## read in data
-# sockeye <- read.csv("https://raw.githubusercontent.com/lizallyn/Pinniped-Case-Studies/main/Data/Ballard%20Daily%20Counts%202012-2022.csv")
+sockeye <- read.csv("https://raw.githubusercontent.com/lizallyn/Pinniped-Case-Studies/main/Data/Ballard%20Daily%20Counts%202012-2022.csv")
 # sockeye <- read.csv("/Users/lizallyn/Documents/GitHub/Thesis/Pinniped Case Studies/Data/Ballard Daily Counts 2012-2022.csv")
-sockeye <- read_csv(fetchGHdata("Pinniped-Case-Studies", "Data/BallardDailyCounts2012-2022.csv"))
+# sockeye <- read_csv(fetchGHdata("Pinniped-Case-Studies", "Data/BallardDailyCounts2012-2022.csv"))
 
 DayofStudy <- rep(163:275, 11)
 sockeye <- data.frame(cbind(sockeye, DayofStudy))
