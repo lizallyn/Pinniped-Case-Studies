9f55b8ab700a7672f5fddd235db887a78c05f593
Liz Allyn <liz.allyn@allyn.org>
2023-11-06 14:56:57 -0800

    started a new one in R because I'm tired and want to make some thinking progress

---
+++ Models/SocialSeals_v1.R
@@ -0,0 +1,10 @@
+### I give up, let's do it in R for the rest of the day
+
+# Set Parameters
+days <- 5
+num_seals <- 20
+seal_initial_prob_gauntlet <- 0.1
+seal_num_neighbours_2_copy <- 2
+seal_prob_2_copy <- 0.5
+
+# Variable setup
