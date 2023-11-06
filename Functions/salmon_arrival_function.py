3109083191139ea7568c07e9e363114782b46d40
Liz Allyn <liz.allyn@allyn.org>
2023-11-06 14:57:15 -0800

    defeated, abandoned for the day

--- Functions/salmon_arrival_function.py
+++ Functions/salmon_arrival_function.py
@@ -29,14 +29,39 @@ plt.show()
 # plot each year separately
 yr_day_sockeye.plot(kind = "line")
 plt.show()
+
+# create fit to fish function
 from scipy.stats import norm
-y_hat = np.repeat((-1), len(df_sockeye))
+from scipy.stats import poisson
+
+y_hat = np.repeat((-1), len(daily_sockeye["DailyCount"]))
+nll = np.repeat((-1), len(y_hat))
+
 def fit_to_fish(params, data):
   for t in range(len(data)):
     y_hat[t] = norm.pdf(x = t, loc = params[1], scale = params[2]) * params[0]
   print(y_hat)
+  
+  for i in range(len(y_hat)):
+    nll += poisson.pmf(data, mu = y_hat, log = True)
+  print(nll)
+
+poisson.pmf(df_sockeye["DailyCount"], mu = y_hat)
 
 # test function
 params_1 = [73500, 23.6, 14]
-fit_to_fish(params = params_1, data = df_sockeye)
+fit_to_fish(params = params_1, data = daily_sockeye["DailyCount"])
+
+### I've been thoroughly defeated by this for no good reason
+### giving up and just making the predict.fish one with existing parameters 
+### Need to move on!!
+### I'm sure this will be obvious at some point
+
+def predict_fish(params, day, start_day):
+  y_hat = norm.pdf(x = day, loc = params[1] + start_day, scale = params[2]) * params[0]
+  print(y_hat)
+
+# params_1 = [73500, 23.6, 14]
+# predict_fish(params = params_1, day = 200, start_day = 163)
+# it works I think
 
