85e486ca5e7318f12a2b5a68c8ca0465fcc8e018
Liz Allyn <liz.allyn@allyn.org>
2023-11-06 15:23:12 -0800

    recreated python version lickety split

--- Models/SocialSeals_v1.R
+++ Models/SocialSeals_v1.R
@@ -1,10 +1,44 @@
 ### I give up, let's do it in R for the rest of the day
 
+# packages and data
+
+
 # Set Parameters
-days <- 5
+days <- 1
 num_seals <- 20
 seal_initial_prob_gauntlet <- 0.1
+seal_start_loc <- 0
 seal_num_neighbours_2_copy <- 2
 seal_prob_2_copy <- 0.5
 
 # Variable setup
+seal_prob_gauntlet <- array(dim = c(num_seals, days), 
+                            data = rep(seal_initial_prob_gauntlet, 
+                                       num_seals * days))
+seal_forage_loc <- array(dim = c(num_seals, days), 
+                         data = rep(seal_start_loc, 
+                                    num_seals * days))
+
+# Run time loop
+for(t in 1:days) {
+  
+  # decide where each seal goes that day
+  for(seal in 1:num_seals) {
+    if(rnorm(1) < seal_prob_gauntlet[seal,t]) 
+    {seal_forage_loc[seal,t] <- 1} 
+    else seal_forage_loc[seal,t] <- 0
+  }
+  
+  # round of copying
+  for(seal in 1:num_seals) {
+    # if the seal wasn't going to the gauntlet
+    if(seal_forage_loc[seal,t] == 0) {
+      seals_2_copy <- seal_forage_loc[sample(1:num_seals, seal_num_neighbours_2_copy, replace = F)]
+      social_information <- mean(seals_2_copy)
+      if(rnorm(1) < seal_prob_2_copy * social_information) {
+        seal_forage_loc[seal] <- 1
+      }
+    }
+  }
+}
+print(seal_forage_loc)
