7f683a1f405bc690036d5ff0a64a5f65536a5ba2
Liz Allyn <liz.allyn@allyn.org>
2023-11-07 09:33:45 -0800

    started adding salmon bits

--- Models/SocialSeals_v1.R
+++ Models/SocialSeals_v1.R
@@ -7,16 +7,23 @@
 source("https://raw.githubusercontent.com/lizallyn/Pinniped-Case-Studies/main/Functions/Sockeye%20arrival%20function%20creation.R")
 
 # Set Parameters
-days <- 1
+days <- 365
 num_seals <- 20
 seal_initial_prob_gauntlet <- 0.1
 seal_start_loc <- 0
 seal_num_neighbours_2_copy <- 2
 seal_prob_2_copy <- 0.5
 prob_seal_forage_success <- 0.3
+max_salmon <- max(sockeye$DailyCount)
+
+escape_rate <- 0.3
 
 # Variable setup
-salmon_arrive <- rep(NA, days)
+salmon_escape <- rep(NA, days)
+salmon_escape[1] <- 0
+gauntlet_salmon <- rep(NA, days)
+gauntlet_salmon[1] <- 0
+salmon_consumed <- rep(NA, num_seals)
 seal_prob_gauntlet <- array(dim = c(num_seals, days), 
                             data = rep(seal_initial_prob_gauntlet, 
                                        num_seals * days))
@@ -25,9 +32,12 @@ seal_forage_loc <- array(dim = c(num_seals, days),
                                     num_seals * days))
 
 # Run time loop
-for(t in 1:days) {
+for(t in 2:days) {
   
-  salmon_arrive[t] <- predict.fish(day = t, params = fish.fit.optim$par, start.day = 163)
+  # salmon at the gauntlet on that day=arrive-leave
+  salmon_arrive <- round(predict.fish(day = t, params = fish.fit.optim$par, start.day = 163), digits = 0)
+  gauntlet_salmon[t] <- gauntlet_salmon[t-1] + salmon_arrive - salmon_escape[t-1]
+  salmon_escape[t] <- gauntlet_salmon[t] * escape_rate
   
   # decide where each seal goes that day
   for(seal in 1:num_seals) {
@@ -49,11 +59,12 @@ for(t in 1:days) {
   }
   
   # round of eating
-  prob_seal_encounter_salmon <- salmon_arrive[t]/
-  # not sure what this should be dependent on yet, probably density and competition
   for(seal in 1:num_seals) {
-    salmon_consumed[seal] <- seal_forage_loc[seal] * prob_seal_encounter_salmon * prob_seal_forage_success[seal]
+    prob_seal_encounter_salmon <- gauntlet_salmon[t]/max_salmon
+    prob_seal_forage_success <- rnorm(n = 1, mean = 20, sd = 7) #Freeman et al 2022
+    salmon_consumed[seal] <- seal_forage_loc[seal] * prob_seal_encounter_salmon *
+      gauntlet_salmon[t] * prob_seal_forage_success
   }
 
 }
-print(seal_forage_loc)
+print(salmon_consumed)
