2ffd3ed549261cb0ee28b54753a5f0f622d50628
Liz Allyn <liz.allyn@allyn.org>
2023-12-06 11:44:48 -0800

    that wasn't the problem

--- Functions/eat_some_fish.R
+++ Functions/eat_some_fish.R
@@ -6,7 +6,7 @@ eat_some_fish <- function(gauntlet_salmon, num_seals_at_gauntlet, handling_time)
   } else {
     salmon_to_be_eaten <- gauntlet_salmon * 
       (num_seals_at_gauntlet / (1 + num_seals_at_gauntlet + 
-                                      seal_handling_time * gauntlet_salmon))
+                                      handling_time * gauntlet_salmon))
     salmon_per_seal <- salmon_to_be_eaten / num_seals_at_gauntlet
     salmon_consumed <- rpois(num_seals_at_gauntlet, salmon_per_seal)
   }
