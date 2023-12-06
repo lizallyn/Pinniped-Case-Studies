d7164067fc20f21ebeb76c8d6fbd0ad032e348f9
Liz Allyn <liz.allyn@allyn.org>
2023-12-05 16:06:18 -0800

    created separate function to decide on foraging loc considering prob gauntlet

---
+++ Functions/decide_foraging_destination.R
@@ -0,0 +1,10 @@
+# create decide_foraging_destination function
+
+decide_foraging_destination <- function(prob_gauntlet) {
+  if(runif(1, 0, 1) < prob_gauntlet) {
+    seal_forage_loc <- 1
+  } else seal_forage_loc <- 0
+  return(seal_forage_loc)
+}
+
+
