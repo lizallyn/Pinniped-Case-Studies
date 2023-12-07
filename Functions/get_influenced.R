01add9fdfd210d44bfdf2bf8fdf6b5056ae6e793
Liz Allyn <liz.allyn@allyn.org>
2023-12-06 16:43:38 -0800

    big typo

--- Functions/get_influenced.R
+++ Functions/get_influenced.R
@@ -5,7 +5,7 @@ get_influenced <- function(seal_num_neighbours_2_copy, num_seals, forage_locs_li
   seals_to_be_influenced <- which(forage_locs_list == 0)
   loc_of_seals_2_copy <- forage_locs_list[sample(1:num_seals, seal_num_neighbours_2_copy, replace = F)]
   social_info <- mean(loc_of_seals_2_copy)
-  if(runif(1, 0, 1) < (seal_num_neighbours_2_copy * social_info)) {
+  if(runif(1, 0, 1) < (seal_prob_2_copy * social_info)) {
     forage_locs_list[seals_to_be_influenced] <- 1
   }
   return(forage_locs_list)
