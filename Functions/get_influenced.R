fdd559fe0790ef7a839bf70ebb9147e09e5c8f39
Liz Allyn <liz.allyn@allyn.org>
2023-12-06 16:40:44 -0800

    maybe get_influenced is ready for a test

--- Functions/get_influenced.R
+++ Functions/get_influenced.R
@@ -1,21 +1,14 @@
 # create get_influenced function
 # not finished - realize this is going to go through some iterations still I bet
 
-for(seal in 1:num_seals) {
-  # if the seal wasn't going to the gauntlet
-  # if(seal_forage_loc[seal,t, y] == 0) {
-  loc_of_seals_2_copy <- seal_forage_loc[sample(1:num_seals, seal_num_neighbours_2_copy, replace = F),t, y]
-  social_information <- mean(loc_of_seals_2_copy)
-  if(runif(1, 0, 1) < (seal_prob_2_copy * social_information)) {
-    seal_forage_loc[seal,t, y] <- 1
-    # }
+get_influenced <- function(seal_num_neighbours_2_copy, num_seals, forage_locs_list, prob_2_copy) {
+  seals_to_be_influenced <- which(forage_locs_list == 0)
+  loc_of_seals_2_copy <- forage_locs_list[sample(1:num_seals, seal_num_neighbours_2_copy, replace = F)]
+  social_info <- mean(loc_of_seals_2_copy)
+  if(runif(1, 0, 1) < (seal_num_neighbours_2_copy * social_info)) {
+    forage_locs_list[seals_to_be_influenced] <- 1
   }
+  return(forage_locs_list)
 }
 
-get_influenced <- function(num_2_copy, num_seals, seal_forage_loc, prob_2_copy) {
-  loc_of_seals_2_copy <- seal_forage_loc[sample(1:num_seals, num_2_copy, replace = F)]
-  social_info <- mean(loc_of_seals_2_copy)
-  if(runif(1, 0, 1) < (prob_2_copy * social_info)) {
-    seal_forage_loc <- social_info
-  }
-}
+# get_influenced(seal_num_neighbours_2_copy = 2, num_seals = 10, forage_locs_list = forage_locs_list_test, prob_2_copy = seal_prob_2_copy)
