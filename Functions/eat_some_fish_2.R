7300bf57d1bbd921d2f3256d01231c294a69e6c1
Liz Allyn <liz.allyn@allyn.org>
2023-12-18 15:18:28 -0800

    changed output so made it into a different file instead

--- Functions/eat_some_fish_2.R
+++ Functions/eat_some_fish_2.R
@@ -2,14 +2,12 @@
 
 eat_some_fish <- function(gauntlet_salmon, num_seals_at_gauntlet, handling_time) {
   if(gauntlet_salmon == 0 | num_seals_at_gauntlet == 0){
-    theoretical_salmon_to_eat <- rep(0, num_seals_at_gauntlet)
+    theoretical_salmon_to_eat <- 0
   } else {
     theoretical_salmon_to_eat <- gauntlet_salmon * 
       (num_seals_at_gauntlet / (1 + num_seals_at_gauntlet + 
                                       handling_time * gauntlet_salmon))
     salmon_per_seal <- theoretical_salmon_to_eat / num_seals_at_gauntlet
-    salmon_consumed <- round(runif(num_seals_at_gauntlet, min = 0, max = salmon_per_seal))
   }
   return(theoretical_salmon_to_eat)
 }
-
