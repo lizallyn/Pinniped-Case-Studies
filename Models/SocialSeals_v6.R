7305b05520def1b50e9edb6d0c07d2f7a96e74b5
Liz Allyn <liz.allyn@allyn.org>
2024-01-03 09:44:41 -0800

    making a version with the x thing in it

---
+++ Models/SocialSeals_v6.R
@@ -0,0 +1,14 @@
+### V6
+# Jan 2024
+
+#### Set Up ####
+
+## Load Packages and Data
+library(ggplot2)
+library(tidyr) #formatting for visualization
+
+## Load Function Files
+source("https://raw.githubusercontent.com/lizallyn/Pinniped-Case-Studies/main/Functions/salmon_arrive.R")
+source("https://raw.githubusercontent.com/lizallyn/Pinniped-Case-Studies/main/Functions/eat_some_fish_2.R")
+source("https://raw.githubusercontent.com/lizallyn/Pinniped-Case-Studies/main/Functions/decide_foraging_destination.R")
+source("https://raw.githubusercontent.com/lizallyn/Pinniped-Case-Studies/main/Functions/get_influenced.R")
