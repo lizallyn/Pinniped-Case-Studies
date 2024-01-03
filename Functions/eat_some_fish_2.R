c215415c3076b84694d01df9efc4ec98d37530c4
Liz Allyn <liz.allyn@allyn.org>
2024-01-03 10:32:15 -0800

    just deleted satiation?

--- Functions/eat_some_fish_2.R
+++ Functions/eat_some_fish_2.R
@@ -8,7 +8,6 @@ eat_some_fish <- function(gauntlet_salmon, num_seals_at_gauntlet, handling_time)
       (num_seals_at_gauntlet / (1 + num_seals_at_gauntlet + 
                                   handling_time * gauntlet_salmon))
     salmon_per_seal <- theoretical_salmon_to_eat / num_seals_at_gauntlet
-    # prop_of_satiation <- salmon_per_seal/satiation
   }
   return(theoretical_salmon_to_eat)
 }
