9a651f21a6713cf2862ff756c3eb3080131b7824
Liz Allyn <liz.allyn@allyn.org>
2024-01-02 17:09:47 -0800

    messed with some stuff, ends basically the same

--- Functions/eat_some_fish_2.R
+++ Functions/eat_some_fish_2.R
@@ -1,13 +1,14 @@
 # create eat_some_fish function
 
-eat_some_fish <- function(gauntlet_salmon, num_seals_at_gauntlet, handling_time) {
+eat_some_fish <- function(gauntlet_salmon, num_seals_at_gauntlet, handling_time, satiation) {
   if(gauntlet_salmon == 0 | num_seals_at_gauntlet == 0){
     theoretical_salmon_to_eat <- 0
   } else {
-    theoretical_salmon_to_eat <- gauntlet_salmon * 
+    theoretical_salmon_to_eat <- gauntlet_salmon *
       (num_seals_at_gauntlet / (1 + num_seals_at_gauntlet + 
-                                      handling_time * gauntlet_salmon))
+                                  handling_time * gauntlet_salmon))
     salmon_per_seal <- theoretical_salmon_to_eat / num_seals_at_gauntlet
+    prop_of_satiation <- salmon_per_seal/satiation
   }
   return(theoretical_salmon_to_eat)
 }
