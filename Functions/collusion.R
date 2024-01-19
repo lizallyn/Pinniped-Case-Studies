ebbc97f4dcd7247be2f10d65817f29c1d686907f
Liz Allyn <liz.allyn@allyn.org>
2024-01-19 14:57:17 -0800

    this maybe works? I z-scored the receptivity term so I think now it seems like it should be ok, but need to do a lot more testing!!!

--- Functions/collusion.R
+++ Functions/collusion.R
@@ -1,15 +1,16 @@
 # community collusion
 
-collusion <- function(forage_locs_list, prob_gauntlet_seal_i, receptivity, mean, beta) {
+collusion <- function(forage_locs_list, prob_gauntlet_seal_i, mean, beta) {
   alpha <- (-beta*mean)/(mean-1)
   receptivity <- dbeta(x = prob_gauntlet_seal_i, shape1 = alpha + 1, shape2 = beta + 1, ncp = 0)
-  P_social <- (1-alpha) * prob_gauntlet_seal_i + alpha * mean(forage_locs_list[,t,j])
+  max <- dbeta(x = mean, shape1 = alpha + 1, shape2 = beta + 1, ncp = 0)
+  min <- 0
+  scaled_rec <- (receptivity - 0)/(max - min)
+  P_social <- (1-scaled_rec) * prob_gauntlet_seal_i + scaled_rec * mean(forage_locs_list[,t,j])
   return(P_social)
 }
 
-collusion(seal_prob_gauntlet, 0.1, )
+collusion(forage_locs_list = seal_prob_gauntlet, prob_gauntlet_seal_i = 0.2, mean = 0.5, beta = 15)
 
-mean <- 0.5
-beta <- 15
-alpha <- (-beta*mean)/(mean-1)
-dbeta(x = 0.2, shape1 = alpha + 1, shape2 = beta + 1, ncp = 0)
+receptivity <- dbeta(x = 0, shape1 = alpha + 1, shape2 = beta + 1, ncp = 0)
+(1-receptivity) * 0.5 + receptivity * 0.1
