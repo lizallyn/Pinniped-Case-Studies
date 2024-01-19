1330d1b73dcfccea3a130243b54bfb461ee2f1cc
Liz Allyn <liz.allyn@allyn.org>
2024-01-19 15:02:29 -0800

    end of day

--- Functions/collusion.R
+++ Functions/collusion.R
@@ -6,11 +6,8 @@ collusion <- function(forage_locs_list, prob_gauntlet_seal_i, mean, beta) {
   max <- dbeta(x = mean, shape1 = alpha + 1, shape2 = beta + 1, ncp = 0)
   min <- 0
   scaled_rec <- (receptivity - 0)/(max - min)
-  P_social <- (1-scaled_rec) * prob_gauntlet_seal_i + scaled_rec * mean(forage_locs_list[,t,j])
+  P_social <- (1-scaled_rec) * prob_gauntlet_seal_i + scaled_rec * mean(forage_locs_list)
   return(P_social)
 }
 
-collusion(forage_locs_list = seal_prob_gauntlet, prob_gauntlet_seal_i = 0.2, mean = 0.5, beta = 15)
 
-receptivity <- dbeta(x = 0, shape1 = alpha + 1, shape2 = beta + 1, ncp = 0)
-(1-receptivity) * 0.5 + receptivity * 0.1
