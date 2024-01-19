196ce059133b084c95d54dcfc030704ef166a690
Liz Allyn <liz.allyn@allyn.org>
2024-01-19 14:46:25 -0800

    using a beta distribution to figure out receptivity based on individual learning

--- Functions/collusion.R
+++ Functions/collusion.R
@@ -1,6 +1,15 @@
 # community collusion
 
-collusion <- function(forage_locs_list, prob_gauntlet_seal_i, alpha){
+collusion <- function(forage_locs_list, prob_gauntlet_seal_i, receptivity, mean, beta) {
+  alpha <- (-beta*mean)/(mean-1)
+  receptivity <- dbeta(x = prob_gauntlet_seal_i, shape1 = alpha + 1, shape2 = beta + 1, ncp = 0)
   P_social <- (1-alpha) * prob_gauntlet_seal_i + alpha * mean(forage_locs_list[,t,j])
   return(P_social)
 }
+
+collusion(seal_prob_gauntlet, 0.1, )
+
+mean <- 0.5
+beta <- 15
+alpha <- (-beta*mean)/(mean-1)
+dbeta(x = 0.2, shape1 = alpha + 1, shape2 = beta + 1, ncp = 0)
