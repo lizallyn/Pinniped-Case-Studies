7521536f0d28323f2ec882f024b7c41ae919210a
Liz Allyn <liz.allyn@allyn.org>
2024-01-19 14:09:30 -0800

    sloppy, fixed some stuff and made it spit out predation rate not consumed count

--- Functions/eat_some_fish_3.R
+++ Functions/eat_some_fish_3.R
@@ -1,12 +1,12 @@
 # new version of consumption with satiation etc.
 
-eat_some_fish <- function(gauntlet_salmon, num_seals_at_gauntlet, handling_time, satiation, pd, Y) {
-  if(gauntlet_salmon == 0 | num_seals_at_gauntlet == 0){
-    theoretical_salmon_to_eat <- 0
+eat_some_fish <- function(num_gauntlet_salmon, num_seals_at_gauntlet, handling_time, satiation, pd, Y) {
+  if(num_gauntlet_salmon == 0 | num_seals_at_gauntlet == 0){
+    theoretical_predation_rate <- 0
   } else {
-    theoretical_salmon_to_eat <- (satiation*handling_time*num_seals_at_gauntlet^(1+pd)*gauntlet_salmon)/
-      (satiation + handling_time*gauntlet_salmon*num_seals_at_gauntlet^pd+Y)
-    salmon_per_seal <- theoretical_salmon_to_eat / num_seals_at_gauntlet
+    theoretical_salmon_to_eat <- (satiation*handling_time*num_seals_at_gauntlet^(1+pd)*num_gauntlet_salmon)/
+      (satiation + handling_time*num_gauntlet_salmon*num_seals_at_gauntlet^pd+Y)
+    theoretical_predation_rate <- theoretical_salmon_to_eat/num_gauntlet_salmon
   }
-  return(theoretical_salmon_to_eat)
+  return(theoretical_predation_rate)
 }
