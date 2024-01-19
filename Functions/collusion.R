bc6d2b63a1860f9e97be4d6f60741f0ab0123962
Liz Allyn <liz.allyn@allyn.org>
2024-01-19 14:32:44 -0800

    simpler copying for now

--- Functions/collusion.R
+++ Functions/collusion.R
@@ -1,9 +1,6 @@
 # community collusion
 
-collusion <- function(seal_gauntlet_probs, num_seals, seal_num_neighbours_2_copy, seal_prob_2_copy){
-  social_info <- mean(forage_locs_list[
-    sample(1:num_seals, seal_num_neighbours_2_copy, replace = F)]) * seal_prob_2_copy
-  if(runif(1, 0, 1) < social_info) {
-    return(1)
-  } else {return(0)}
+collusion <- function(forage_locs_list, prob_gauntlet_seal_i, alpha){
+  P_social <- (1-alpha) * prob_gauntlet_seal_i + alpha * mean(forage_locs_list[,t,j])
+  return(P_social)
 }
