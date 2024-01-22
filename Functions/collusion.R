0d3484855b8fad110b2d376faed29837fe76c12d
Liz Allyn <liz.allyn@allyn.org>
2024-01-22 12:19:08 -0800

    made it so they learn from a set number of randomly drawn neighbors instead of everyone

--- Functions/collusion.R
+++ Functions/collusion.R
@@ -1,12 +1,14 @@
 # community collusion
 
-collusion <- function(forage_locs_list, prob_gauntlet_seal_i, mean, beta) {
+collusion <- function(probs_list, seals_2_copy, mean, beta) {
   alpha <- (-beta*mean)/(mean-1)
-  receptivity <- dbeta(x = prob_gauntlet_seal_i, shape1 = alpha + 1, shape2 = beta + 1, ncp = 0)
+  receptivity <- dbeta(x = probs_list[i], shape1 = alpha + 1, shape2 = beta + 1, ncp = 0)
   max <- dbeta(x = mean, shape1 = alpha + 1, shape2 = beta + 1, ncp = 0)
   min <- 0
   scaled_rec <- (receptivity - 0)/(max - min)
-  P_social <- (1-scaled_rec) * prob_gauntlet_seal_i + scaled_rec * mean(forage_locs_list)
+  social_info <- mean(probs_list[
+    sample(1:nrow(probs_list), seals_2_copy, replace = F)])
+  P_social <- (1-scaled_rec) * probs_list[i] + scaled_rec * social_info
   return(P_social)
 }
 
