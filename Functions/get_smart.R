f7257f7a3d8fd4cade6dbfb5ed85a1464270d4e0
Liz Allyn <liz.allyn@allyn.org>
2024-01-24 15:13:24 -0800

    trying to test - something weird going on where any hunting immediately drops P to 0

--- Functions/get_smart.R
+++ Functions/get_smart.R
@@ -1,6 +1,7 @@
 # individual learning function with x and y
 
-get_smart <- function(salmon_eaten, hunt_exposure, satiation, baseline, constraints, x, y, steepness, threshold, step){
+get_smart <- function(salmon_eaten, hunt_exposure, forage_loc, satiation, baseline, 
+                      constraints, x, y, steepness, threshold, step, decay){
   xmin <- constraints[1]
   xmax <- constraints[2]
   ymin <- constraints[3]
@@ -9,10 +10,16 @@ get_smart <- function(salmon_eaten, hunt_exposure, satiation, baseline, constrai
   C <- salmon_eaten/satiation - baseline
   
   # calculate x and P_x
-  if(C > 0){
-    d_x <- 0.25*(xmax - x)
+  if(forage_loc == 0) {
+    
+    if(x == 0){
+      d_x <- 0
+    } else {d_x <- (x-0)/abs(x)}
+    
+  } else if(C > 0){
+    d_x <- step*(xmax - x)
   } else if(C < 0){
-    d_x <- 0.25*(xmin - x)
+    d_x <- step*(xmin - x)
   } else {d_x <- 0}
   x_next <- x + d_x
   P_x <- x_next * 0.1 + 0.1
@@ -20,7 +27,7 @@ get_smart <- function(salmon_eaten, hunt_exposure, satiation, baseline, constrai
   # calculate y and P_y
   if(forage_loc == 0) {
     
-    if(y > 0.9){
+    if(y == 0){
       d_y <- 0
     } else {d_y <- decay}
     
@@ -36,5 +43,23 @@ get_smart <- function(salmon_eaten, hunt_exposure, satiation, baseline, constrai
   return(P_gauntlet)
 }
 
-setp = 0.25
-decay = 0.05
+## Testing space
+
+# step = 0.25
+# decay = 0.25
+# 
+# z <- -10:10
+# plot(-10:10, z - (z-0)/abs(z))
+
+salmon <- 0:19
+hunting <- c(rep(0, 5), 1:5, 5:1, rep(0, 5))
+data <- rep(NA, 20)
+for(i in 1:20) {
+  data[i] <- get_smart(salmon_eaten = salmon[i], hunt_exposure = hunting[i], forage_loc = 1, 
+                    satiation = 3, baseline = 1, constraints = c(-1, 9, -10, 0), 
+                    x = 0, y = -5, steepness = 5, threshold = -5, step = 0.25, decay = 0.25)
+}
+par(mfrow = c(3, 1))
+plot(1:20, data)
+plot(1:20, salmon, main = "salmon")
+plot(1:20, hunting, main = "hunting")
