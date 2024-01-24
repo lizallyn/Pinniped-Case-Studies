f32622c891a678d88c6e206ec8e7c27c241caf87
Liz Allyn <liz.allyn@allyn.org>
2024-01-24 15:54:27 -0800

    Pulled more constants out into named parameters

--- Models/SocialSeals_v7.R
+++ Models/SocialSeals_v7.R
@@ -20,7 +20,7 @@ years <- 1
 days <- 365
 
 # seal parameters
-num_seals <- 2
+num_seals <- 5
 seal_initial_prob_gauntlet <- 0.1
 seal_start_loc <- 0
 
@@ -39,11 +39,15 @@ ymin <- -10
 ymax <- 0
 xmin <- -1
 xmax <- 9
-steepness <- 5
-threshold <- -3
+steepness <- 1
+threshold <- -5
+slope_x <- 0.1
+intercept_x <- 0.1
+step <- 0.25
+buffer_Pymin <- 0.1
 
 # seal social learning parameters
-num_seals_2_copy <- 0
+num_seals_2_copy <- 5
 mean <- 0.5 # of the beta dist
 beta <- 15 # spread of the beta dist
 
@@ -143,23 +147,24 @@ for(j in 1:years) {
     for(seal in 1:num_seals){
       C[seal, t, j] <- salmon_consumed[seal, t, j]/seal_satiation - w
       if(C[seal, t, j] > 0){
-        d_x <- 0.25*(xmax - x[seal, t, j])
+        d_x <- step*(xmax - x[seal, t, j])
       } else if(C[seal, t, j] < 0){
-        d_x <- 0.25*(xmin - x[seal, t, j])
+        d_x <- step*(xmin - x[seal, t, j])
       } else {d_x <- 0}
       x[seal, t+1, j] <- x[seal, t+1, j] + d_x
-      P_x[seal, t+1, j] <- x[seal, t+1, j] * 0.1 + 0.1
+      P_x[seal, t+1, j] <- x[seal, t+1, j] * slope_x + intercept_x
       
       if(sum(H[t, j]) == 0){
-        d_y <- 0.25*(ymax - y[seal, t, j])
+        d_y <- step*(ymax - y[seal, t, j])
       } else if(sum(H[t, j]) > 0){
-        d_y <- 0.25*(ymin - y[seal, t, j])
+        d_y <- step*(ymin - y[seal, t, j])
       }
       y[seal, t+1, j] <- y[seal, t, j] + d_y
-      P_y[seal, t+1, j] <- 1-(1/(1.1 + exp(-steepness * (threshold - y[seal, t+1, j]))))
+      P_y[seal, t+1, j] <- 1-(1/((1+buffer_Pymin) + exp(-steepness * (threshold - y[seal, t+1, j]))))
       
       seal_prob_gauntlet[seal, t+1, j] <- P_x[seal, t+1, j] * P_y[seal, t+1, j]
-      }
+    }
+    
     
   } # days loop
 } # years loop
