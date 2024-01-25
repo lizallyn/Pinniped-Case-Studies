0db5c7265e07aeb6d2a04a034ae3ab7e6d1aceb5
Liz Allyn <liz.allyn@allyn.org>
2024-01-25 10:44:56 -0800

    function for updating y

---
+++ Functions/learn_y.R
@@ -0,0 +1,15 @@
+# function for calculating d_y if they go to the gauntlet
+
+learn_y <- function(hunting, y_t, forage_loc, step, ymin, ymax, decay) {
+  if(forage_loc == 0){
+    d_y <- decay
+  } else {
+    if(hunting == 0){
+      d_y <- step*(ymax - y_t)
+    } else if(hunting > 0){
+      d_y <- step*(ymin - y_t)
+    }
+  }
+  
+  return(d_y)
+}
