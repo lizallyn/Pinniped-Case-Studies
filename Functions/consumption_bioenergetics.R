6fb5487f20a74f5e265cb3c0252964e57eaa6013
Liz Allyn <liz.allyn@allyn.org>
2024-01-25 17:03:41 -0800

    started thinking about this, gonna take some lit spelunking

--- Functions/consumption_bioenergetics.R
+++ Functions/consumption_bioenergetics.R
@@ -1,3 +1,14 @@
 # Bioenergetics of consumption
 # from conversation with Isaac on Dec 7, 2023
+# and the need to use weight in Tim's consumption equation
+
+# average Pv kg
+female <- 66
+male <- 89
+
+sockeye <- 6
+chinook <- 8
+coho <- 4
+
+eat_some_fish(num_gauntlet_salmon = )
 
