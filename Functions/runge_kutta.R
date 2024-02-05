3093c89c9a5188e9527807718a9446410970833a
Liz Allyn <liz.allyn@allyn.org>
2024-02-05 09:55:40 -0800

    change F to F_catch to avoid ambiguity

--- Functions/runge_kutta.R
+++ Functions/runge_kutta.R
@@ -1,15 +1,15 @@
 # apply 4th order runge-kutta to integrate over time period deltat
 # From Tim Feb 2024
 
-runge_kutta <- function(Cmax, Nseal, alpha, Ns, gamma, Y, F, M, E, deltat = 1){
+runge_kutta <- function(Cmax, Nseal, alpha, Ns, gamma, Y, F_catch, M, E, deltat = 1){
   X <- c(Ns, 0, 0, 0)
-  K1s <- get_dXdt(Cmax, Nseal, alpha, Ns = X[1], gamma, Y, F, M, E)
+  K1s <- get_dXdt(Cmax, Nseal, alpha, Ns = X[1], gamma, Y, F_catch, M, E)
   midX <- X + deltat* 0.5 * K1s
-  K2s <- get_dXdt(Cmax, Nseal, alpha, Ns= midX[1], gamma, Y, F, M, E)
+  K2s <- get_dXdt(Cmax, Nseal, alpha, Ns= midX[1], gamma, Y, F_catch, M, E)
   midX <- X + deltat * 0.5 * K2s
-  K3s <- get_dXdt(Cmax, Nseal, alpha, Ns= midX[1], gamma, Y, F, M, E)
+  K3s <- get_dXdt(Cmax, Nseal, alpha, Ns= midX[1], gamma, Y, F_catch, M, E)
   endX <- X + deltat * K3s
-  K4s <- get_dXdt(Cmax, Nseal, alpha, Ns= endX[1], gamma, Y, F, M, E)
+  K4s <- get_dXdt(Cmax, Nseal, alpha, Ns= endX[1], gamma, Y, F_catch, M, E)
   Xsim <- X + deltat * (K1s / 6 + K2s / 3 + K3s / 3 + K4s / 6)
   return(c(Ns = Xsim[1],
            C = Xsim[2],
