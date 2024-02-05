8d7c2ee8872c4f3c716c73f33c01f60370901dff
Liz Allyn <liz.allyn@allyn.org>
2024-02-05 15:51:41 -0800

    added the ability to return destination "NA" for dead individuals with prob "NA"

--- Functions/decide_foraging_destination.R
+++ Functions/decide_foraging_destination.R
@@ -1,10 +1,12 @@
 # create decide_foraging_destination function
 
 decide_foraging_destination <- function(prob_gauntlet) {
-  if(runif(1, 0, 1) < prob_gauntlet) {
+  if(is.na(prob_gauntlet)) {
+    seal_forage_dest <- NA
+  } else if(runif(1, 0, 1) < prob_gauntlet) {
     seal_forage_dest <- 1
-  } else seal_forage_dest <- 0
+  } else {seal_forage_dest <- 0}
+  
   return(seal_forage_dest)
 }
-
 
