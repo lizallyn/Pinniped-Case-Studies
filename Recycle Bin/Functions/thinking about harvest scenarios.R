42277d68b4d40497a50c49e6ba5621d30a77a8bc
Liz Allyn <liz.allyn@allyn.org>
2024-02-05 13:48:16 -0800

    thinking about boat-based harvest representations

--- Recycle Bin/Functions/thinking about harvest scenarios.R
+++ Recycle Bin/Functions/thinking about harvest scenarios.R
@@ -10,3 +10,16 @@ if(gauntlet_salmon[t, j] > 0) {
 
 ## Fishers Take as Observed
 
+# if like consumption:
+H[t, j] <- (Hmax * handling_time * num_gillnetters^(1+gamma) * num_gauntlet_seals)/
+  (Hmax + handling_time * num_gauntlet_seals * num_gillnetters^gamma + Y)
+
+Hmax <- 2
+handling_time <- 0.05
+num_gillnetters <- 10
+gamma <- 0
+num_gauntlet_seals <- 20
+Y <- 0
+
+(Hmax * handling_time * num_gillnetters^(1+gamma) * num_gauntlet_seals)/
+  (Hmax + handling_time * num_gauntlet_seals * num_gillnetters^gamma + Y)
