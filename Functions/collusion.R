6095213fabd4511d581d75cc48f990dc84562600
Liz Allyn <liz.allyn@allyn.org>
2024-02-05 16:04:52 -0800

    it should be able to handle NA probs now?

--- Functions/collusion.R
+++ Functions/collusion.R
@@ -7,8 +7,13 @@ collusion <- function(probs_list, prob_gauntlet_of_seal, seals_2_copy, mean, bet
   min <- 0
   scaled_rec <- (receptivity - 0)/(max - min)
   social_info <- mean(probs_list[
-    sample(1:length(probs_list), seals_2_copy, replace = F)])
-  P_social <- (1-scaled_rec) * prob_gauntlet_of_seal + scaled_rec * social_info
+    sample(1:length(probs_list), seals_2_copy, replace = F)], na.rm = T)
+  if(is.nan(social_info)){
+    P_social <- NA
+  } else {
+    P_social <- (1-scaled_rec) * prob_gauntlet_of_seal + scaled_rec * social_info
+  }
+ 
   return(P_social)
 }
 
