ff902544b739e6cd4cee02c236dde6576829865c
Liz Allyn <liz.allyn@allyn.org>
2024-02-12 14:19:13 -0800

    progress

--- Recycle Bin/Skills/Shiny/app.R
+++ Recycle Bin/Skills/Shiny/app.R
@@ -1,7 +1,16 @@
 library(shiny)
+
 ui <- fluidPage(
-  "Hello, world!"
+  sliderInput("x", label = "If x is", min = 1, max = 50, value = 30),
+  sliderInput("y", label = "and y is", min = 1, max = 50, value = 30),
+  "then x times y is",
+  textOutput("product")
 )
+
 server <- function(input, output, session) {
+  output$product <- renderText({
+    input$x * input$y
+  })
 }
+
 shinyApp(ui, server)
