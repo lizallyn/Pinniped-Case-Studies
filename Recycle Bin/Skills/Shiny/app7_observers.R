59bcbe95acd0b01851a637efb9da8b2d96b034fc
Liz Allyn <liz.allyn@allyn.org>
2024-02-13 11:12:17 -0800

    observers

---
+++ Recycle Bin/Skills/Shiny/app7_observers.R
@@ -0,0 +1,19 @@
+# observers
+
+library(shiny)
+
+ui <- fluidPage(
+  textInput("name", "What's your name?"),
+  textOutput("greeting")
+)
+
+server <- function(input, output, session) {
+  string <- reactive(paste0("Hello ", input$name, "!"))
+  
+  output$greeting <- renderText(string())
+  observeEvent(input$name, {
+    message("Greeting performed") # prints message to the console
+  })
+}
+
+shinyApp(ui, server)
