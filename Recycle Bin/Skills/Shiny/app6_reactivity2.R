64f916807a778977dca7eed64460b24e4ae99722
Liz Allyn <liz.allyn@allyn.org>
2024-02-13 11:11:04 -0800

    reactivity part 2

---
+++ Recycle Bin/Skills/Shiny/app6_reactivity2.R
@@ -0,0 +1,32 @@
+# reactivity continued
+
+library(shiny)
+
+ui <- fluidPage(
+  fluidRow(
+    column(3, 
+           numericInput("lambda1", label = "lambda1", value = 3),
+           numericInput("lambda2", label = "lambda2", value = 5),
+           numericInput("n", label = "n", value = 1e4, min = 0),
+           actionButton("simulate", "Simulate!")
+    ),
+    column(9, plotOutput("hist"))
+  )
+)
+
+server <- function(input, output, session) {
+  # eventReactive() separates the dependency from the values required for computation
+  # this will only reevaluate when button pushed, not when values change
+  x1 <- eventReactive(input$simulate, {
+    rpois(input$n, input$lambda1)
+  })
+  x2 <- eventReactive(input$simulate, {
+    rpois(input$n, input$lambda2)
+  })
+  
+  output$hist <- renderPlot({
+    freqpoly(x1(), x2(), binwidth = 1, xlim = c(0, 40))
+  }, res = 96)
+}
+
+shinyApp(ui, server)
