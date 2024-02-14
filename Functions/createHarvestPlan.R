4842fa77e8096421617170c1052500b0de3fd70a
Liz Allyn <liz.allyn@allyn.org>
2024-02-14 14:42:28 -0800

    cleanup

--- Functions/createHarvestPlan.R
+++ Functions/createHarvestPlan.R
@@ -3,7 +3,7 @@
 source("https://raw.githubusercontent.com/lizallyn/Pinniped-Case-Studies/main/Functions/Prep_data_for_Harvest_functions.R")
 
 ### No harvest
-createHarvestPlan <- function(scenario = c("None", "Zone", "Boat"), days, years, zone_rate = NA, salmon_days = NA, boat_days = NA, boat_rate = NA) {
+createHarvestPlan <- function(scenario = c("None", "Zone", "Boat"), days, years, salmon_days = NA, boat_days = NA) {
   harvest_plan <- makeArray(days, years, start.val = 0, namex = "Day", namey = "Year")
   if(scenario == "None") {
     harvest_plan <- harvest_plan
