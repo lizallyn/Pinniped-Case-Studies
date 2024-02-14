f8c4cd0862034cf7b9052b2396750e655f0eef57
Liz Allyn <liz.allyn@allyn.org>
2024-02-14 14:24:13 -0800

    seems to be working

--- Functions/createHarvestPlan.R
+++ Functions/createHarvestPlan.R
@@ -1,19 +1,19 @@
 # Create some harvest_plan scenarios
 
-source()
+source("https://raw.githubusercontent.com/lizallyn/Pinniped-Case-Studies/main/Functions/Prep_data_for_Harvest_functions.R")
 
 ### No harvest
-createHarvestPlan <- function(scenario = c("None", "Zone", "Boat"), days, years, zone_rate = NA, salmon_days = NA, boat_data, boat_rate = NA) {
+createHarvestPlan <- function(scenario = c("None", "Zone", "Boat"), days, years, zone_rate = NA, salmon_days = NA, boat_days = NA, boat_rate = NA) {
+  harvest_plan <- makeArray(days, years, start.val = 0, namex = "Day", namey = "Year")
   if(scenario == "None") {
-    harvest_plan <- makeArray(days, years, 0, "Day", "Year")
+    harvest_plan <- harvest_plan
   } else if(scenario == "Zone") {
-    harvest_plan <- makeArray(days, years, 0, "Day", "Year")
     harvest_plan[salmon_days,] <- zone_rate
   } else if(scenario == "Boat") {
-    
+    harvest_plan[boat_days,] <- boat_rate
   } else {
     return("Not a valid scenario option. Try: None, Zone, Boat.")
   }
-  }
-  
+  return(harvest_plan)
 }
+
