2640fcad558db5c9194403c4b69f62b2d01b3cb1
lizallyn <lizallyn@uw.edu>
2024-02-15 15:34:08 -0800

    some necessary packages weren't referenced

--- Functions/Prep_data_for_Salmon_functions.R
+++ Functions/Prep_data_for_Salmon_functions.R
@@ -1,4 +1,6 @@
 # Script to prep salmon data for SalmonSpeciesArrive function calls
+library(tidyr)
+library(dplyr)
 
 fish.long <- read.csv("https://raw.githubusercontent.com/lizallyn/Pinniped-Case-Studies/main/Data/Ballard%20Locks%20Fish%20Counts/Ballard%20Locks%20Summarize%20all%20species%20all%20years.csv")
 
