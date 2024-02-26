4d64ff21c9327e858e9df2c06d891cb1260afae3
Liz Allyn <liz.allyn@allyn.org>
2024-02-26 15:44:38 -0800

    fixed to just make a vector

--- Functions/createHarvestPlan.R
+++ Functions/createHarvestPlan.R
@@ -1,14 +1,14 @@
 # Create some harvest_plan scenarios
 
 
-createHarvestPlan <- function(scenario = c("None", "Zone", "Boat"), days, years, salmon_days = NA, boat_days = NA) {
-  harvest_plan <- makeArray(days, years, start.val = 0, namex = "Day", namey = "Year")
+createHarvestPlan <- function(scenario = c("None", "Zone", "Boat"), days, years, salmon_days = NA, boat_days = NA, empty.array = oneDzeroes) {
+  harvest_plan <- empty.array
   if(scenario == "None") {
     harvest_plan <- harvest_plan
   } else if(scenario == "Zone") {
-    harvest_plan[salmon_days,] <- scenario
+    harvest_plan[salmon_days] <- scenario
   } else if(scenario == "Boat") {
-    harvest_plan[boat_days,] <- scenario
+    harvest_plan[boat_days] <- scenario
   } else {
     return("Not a valid scenario option. Try: None, Zone, Boat.")
   }
