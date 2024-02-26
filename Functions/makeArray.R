05a47dae5da45b7d9bd43031891a547009ce651f
Liz Allyn <liz.allyn@allyn.org>
2024-02-26 15:18:41 -0800

    can now handle creating 1D vectors too

--- Functions/makeArray.R
+++ Functions/makeArray.R
@@ -1,18 +1,26 @@
 # function to create arrays for loop variables
 
-makeArray <- function(x, y, z=NA, start.val, namex, namey, namez=NA) {
-  if(is.na(z)){
-    new.array <- array(dim = c(x, y), 
-                       data = rep(start.val, times = x * y),
-                       dimnames = list(namex = 1:x, namey = 1:y))
-    names(dimnames(new.array)) <- c(namex, namey)
-    return(new.array)
+makeArray <- function(dims, start.val, names) {
+  namex <- names[1]
+  namey <- names[2]
+  namez <- names[3]
+  
+  if(length(dims) == 1){
+    new.array <- rep(start.val, dims[1])
     
-  } else {
-    new.array <- array(dim = c(x, y, z), 
-                       data = rep(start.val, times = prod(c(x, y, z), na.rm = T)),
-                       dimnames = list(namex = 1:x, namey = 1:y, namez = 1:z))
+  } else if(length(dims) == 2){
+    new.array <- array(dim = c(dims[1], dims[2]), 
+                       data = rep(start.val, times = dims[1] * dims[2]),
+                       dimnames = list(namex = 1:dims[1], namey = 1:dims[2]))
+    names(dimnames(new.array)) <- c(namex, namey)
+    
+  } else  if(length(dims) == 3){
+    new.array <- array(dim = c(dims[1], dims[2], dims[3]), 
+                       data = rep(start.val, times = prod(c(dims[1], dims[2], dims[3]), na.rm = T)),
+                       dimnames = list(namex = 1:dims[1], namey = 1:dims[2], namez = 1:dims[3]))
     names(dimnames(new.array)) <- c(namex, namey, namez)
-    return(new.array)
   }
+  return(new.array)
 }
+
+
