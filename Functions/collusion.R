59ec88363bf8dc9f81979deda940f274e796aef9
Liz Allyn <liz.allyn@allyn.org>
2024-03-07 09:25:03 -0800

    added error if you try to give to 0 seals to copy

--- Functions/collusion.R
+++ Functions/collusion.R
@@ -1,20 +1,25 @@
 # community collusion
 
 collusion <- function(probs_list, prob_gauntlet_of_seal, seals_2_copy, mean, beta) {
-  alpha <- (-beta*mean)/(mean-1)
-  receptivity <- dbeta(x = prob_gauntlet_of_seal, shape1 = alpha + 1, shape2 = beta + 1, ncp = 0)
-  max <- dbeta(x = mean, shape1 = alpha + 1, shape2 = beta + 1, ncp = 0)
-  min <- 0
-  scaled_rec <- (receptivity - 0)/(max - min)
-  social_info <- mean(probs_list[
-    sample(1:length(probs_list), seals_2_copy, replace = F)], na.rm = T)
-  if(is.nan(social_info)){
-    P_social <- NA
+  if(seals_2_copy == 0){
+    print("No seals to copy :(")
   } else {
-    P_social <- (1-scaled_rec) * prob_gauntlet_of_seal + scaled_rec * social_info
+    alpha <- (-beta*mean)/(mean-1)
+    receptivity <- dbeta(x = prob_gauntlet_of_seal, shape1 = alpha + 1, shape2 = beta + 1, ncp = 0)
+    max <- dbeta(x = mean, shape1 = alpha + 1, shape2 = beta + 1, ncp = 0)
+    min <- 0
+    scaled_rec <- (receptivity - 0)/(max - min)
+    social_info <- mean(probs_list[
+      sample(1:length(probs_list), seals_2_copy, replace = F)], na.rm = T)
+    if(is.nan(social_info)){
+      P_social <- NA
+    } else {
+      P_social <- (1-scaled_rec) * prob_gauntlet_of_seal + scaled_rec * social_info
+    }
+    
+    return(P_social)
   }
- 
-  return(P_social)
+  
 }
 
-
+collusion(c(0.1, 0.2), 0.3, 0, 0.5, 15)
