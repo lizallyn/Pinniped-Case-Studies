4f6095a9060c4741921838722d8a8f8ccb188746
Liz Allyn <liz.allyn@allyn.org>
2024-03-11 16:53:15 -0700

    don't override dead seals

--- Functions/collusion.R
+++ Functions/collusion.R
@@ -3,6 +3,8 @@
 collusion <- function(probs_list, prob_gauntlet_of_seal, seals_2_copy, mean, beta) {
   if(seals_2_copy == 0){
     print("No seals to copy :(")
+  } else if (is.na(prob_gauntlet_of_seal)){
+    P_social <- NA
   } else {
     alpha <- (-beta*mean)/(mean-1)
     receptivity <- dbeta(x = prob_gauntlet_of_seal, shape1 = alpha + 1, shape2 = beta + 1, ncp = 0)
