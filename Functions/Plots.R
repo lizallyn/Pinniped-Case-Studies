989b0521634d5a9d70e59d0c2bd88de8ab2c2e77
Liz Allyn <liz.allyn@allyn.org>
2024-03-11 12:30:49 -0700

    more plots

--- Functions/Plots.R
+++ Functions/Plots.R
@@ -62,6 +62,13 @@ plot_Px <- ggplot(data = Px_plot, aes(x = Day, y = P_x, color = Seal)) +
   scale_color_manual(values = colors)
 plot_Px
 
+seal.data <- data.frame(cbind(1:days, colSums(seal_forage_loc)))
+colnames(seal.data) <- c("Day", "Count")
+plot_seals <- ggplot(data = seal.data, aes(x = Day, y = Count)) +
+  geom_point(color = "dodgerblue") +
+  labs(y = "Num Seals at the Gauntlet")
+plot_seals
+
 H_plot <- data.frame(cbind(1:days, H))
 colnames(H_plot) <- c("Day", "H")
 plot_H <- ggplot(data = H_plot, aes(x = Day, y = H)) +
@@ -80,12 +87,12 @@ plot_Py <- ggplot(data = Py_plot, aes(x = Day, y = P_y, color = Seal)) +
   scale_color_manual(values = colors)
 plot_Py
 
-# each salmon species escaping
+# each salmon species
 
 escape.data <- data.frame(cbind(1:days, escape_chinook, escape_sockeye, escape_coho))
 colnames(escape.data) <- c("Day", "Chinook", "Sockeye", "Coho")
 escape.data <- melt(escape.data, "Day", variable.name = "Species", value.name = "Count")
-salmon.colors <- c("dodgerblue", "salmon", "green3")
+salmon.colors <- c("seagreen", "salmon", "goldenrod")
 salmon.names <- levels(escape.data$Species)
 names(salmon.colors) <- salmon.names
 escape_plot <- ggplot(data = escape.data, aes(x = Day)) +
@@ -93,6 +100,24 @@ escape_plot <- ggplot(data = escape.data, aes(x = Day)) +
   scale_color_manual(values = salmon.colors) +
   labs(y = "Cumulative Salmon Escaped")
 escape_plot
+
+eaten.data <- data.frame(cbind(1:days, eaten_chinook, eaten_sockeye, eaten_coho))
+colnames(eaten.data) <- c("Day", "Chinook", "Sockeye", "Coho")
+eaten.data <- melt(eaten.data, "Day", variable.name = "Species", value.name = "Count")
+eaten_plot <- ggplot(data = eaten.data, aes(x = Day, y = Count)) + 
+  geom_point(aes(color = Species)) +
+  scale_color_manual(values = salmon.colors) +
+  labs(y = "Daily Salmon Eaten")
+eaten_plot
+
+gauntlet.data <- data.frame(cbind(1:days, gauntlet_chinook, gauntlet_sockeye, gauntlet_coho))
+colnames(gauntlet.data) <- c("Day", "Chinook", "Sockeye", "Coho")
+gauntlet.data <- melt(gauntlet.data, "Day", variable.name = "Species", value.name = "Count")
+gauntlet_plot <- ggplot(data = gauntlet.data, aes(x = Day, y = Count)) + 
+  geom_point(aes(color = Species)) +
+  scale_color_manual(values = salmon.colors) +
+  labs(y = "Daily Salmon at Gauntlet")
+gauntlet_plot
 
 ## Composites ----
 
