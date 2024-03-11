a40e41f82bb35952a3e62d5546e5d9ab39f1d544
Liz Allyn <liz.allyn@allyn.org>
2024-03-11 10:31:04 -0700

    fixed magic number

--- Functions/collusion.R
+++ Functions/collusion.R
@@ -8,7 +8,7 @@ collusion <- function(probs_list, prob_gauntlet_of_seal, seals_2_copy, mean, bet
     receptivity <- dbeta(x = prob_gauntlet_of_seal, shape1 = alpha + 1, shape2 = beta + 1, ncp = 0)
     max <- dbeta(x = mean, shape1 = alpha + 1, shape2 = beta + 1, ncp = 0)
     min <- 0
-    scaled_rec <- (receptivity - 0)/(max - min) # fix magic number
+    scaled_rec <- (receptivity - min)/(max - min) # fix magic number
     social_info <- mean(probs_list[
       sample(1:length(probs_list), seals_2_copy, replace = F)], na.rm = T)
     if(is.nan(social_info)){
