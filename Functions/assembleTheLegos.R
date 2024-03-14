c58171cb6cb0bd4580b122683216c53b6864113b
Liz Allyn <liz.allyn@allyn.org>
2024-03-14 11:28:09 -0700

    cleaned up the model run process - not just one function call!

--- Functions/assembleTheLegos.R
+++ Functions/assembleTheLegos.R
@@ -1,34 +1,41 @@
-### This is the Dashboard from which to run the social seals model
+### This function runs the whole thing and can be setup to run different types of runs
 
-## Load Data Files
-source("Functions/Prep_data_for_Salmon_functions.R")
-source("Functions/Prep_data_for_Harvest_functions.R")
+assembleTheLegos <- function(run){
+  ## Load Data Files and Setup Functions 
+  source("Functions/Prep_data_for_Salmon_functions.R")
+  source("Functions/Prep_data_for_Harvest_functions.R")
+  
+  source("Functions/makeArray.R")
+  source("Functions/createHarvestPlan.R")
+  
+  
+## Set Parameters and Create Variables
+  if(run == "Base"){
+    source("Functions/BaseRun_set_pars.R")
+    source("Functions/BaseRun_initialize_variables.R")
+  } else {
+    source("Functions/set_pars.R")
+    source("Functions/initialize_variables.R")
+  }
+  
+  ## Load Function Files
+  source("Functions/makeArray.R")
+  source("Functions/createHarvestPlan.R")
+  source("Functions/salmonSpeciesUpdate.R")
+  source("Functions/decideForagingDestination.R")
+  source("Functions/collusion.R")
+  source("Functions/rungeKutta.R")
+  source("Functions/getHarvested.R")
+  source("Functions/learnX.R")
+  source("Functions/learnY.R")
+  
+  ## Run the Loop
+  source("Models/Run_The_Loop.R")
+  
+  ## Plots
+  source("Functions/Plots.R")
+  
+}
 
-## Load Function Files
-source("Functions/makeArray.R")
-source("Functions/createHarvestPlan.R")
-source("Functions/salmonSpeciesUpdate.R")
-source("Functions/decideForagingDestination.R")
-source("Functions/collusion.R")
-source("Functions/rungeKutta.R")
-source("Functions/getHarvested.R")
-source("Functions/learnX.R")
-source("Functions/learnY.R")
 
-## Load Set Up Files
-source("Functions/BaseRun_set_pars.R")
-source("Functions/BaseRun_initialize_variables.R")
 
-## Run the Loop
-source("Models/Run_The_Loop.R")
-
-## Plots
-source("Functions/Plots.R")
-
-gauntlet_plot / eaten_sp_plot / escape_plot + plot_layout(guides = "collect")
-
-eaten_sp_plot/plot_eaten/plot_C/plot_x/plot_Px + plot_layout(guides = "collect")
-
-plot_seals/plot_H/plot_y/plot_Py + plot_layout(guides = "collect")
-
-plot_Px + plot_Py + plot_probs + plot_layout(guides = "collect")
---
+++ Models/Run_The_Model.R
@@ -0,0 +1,10 @@
+source("Functions/assembleTheLegos.R")
+assemble
+
+gauntlet_plot / eaten_sp_plot / escape_plot + plot_layout(guides = "collect")
+
+eaten_sp_plot/plot_eaten/plot_C/plot_x/plot_Px + plot_layout(guides = "collect")
+
+plot_seals/plot_H/plot_y/plot_Py + plot_layout(guides = "collect")
+
+plot_Px + plot_Py + plot_probs + plot_layout(guides = "collect")
