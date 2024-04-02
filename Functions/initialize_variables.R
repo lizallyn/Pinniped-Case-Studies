67fa5b1953c021708df9bdb1e3169dab03839331
Liz Allyn <liz.allyn@allyn.org>
2024-04-02 10:28:11 -0700

    update for experimental runs

--- Functions/initialize_variables.R
+++ Functions/initialize_variables.R
@@ -1,4 +1,3 @@
-
 ## Set Up Variables ----
 
 oneDzeroes <- makeArray(days, start.val = 0, names = "Day")
@@ -17,6 +16,8 @@ P_y <- twoDzeroes
 
 P_social <- twoDzeroes
 
+kill_list <- c()
+
 ### Actual States that I Need ----
 escape_chinook <- oneDzeroes
 escape_sockeye <- oneDzeroes
@@ -26,13 +27,24 @@ gauntlet_chinook <- oneDzeroes
 gauntlet_sockeye <- oneDzeroes
 gauntlet_coho <- oneDzeroes
 
+fished_chinook <- oneDzeroes
+fished_sockeye <- oneDzeroes
+fished_coho <- oneDzeroes
+
 H <- oneDzeroes
 
 ### Variable Rates ----
 coho_catch_rate <- oneDzeroes
 coho_catch_rate[boat_days] <- coho_fish_rate
 chinook_catch_rate <- oneDzeroes
+chinook_catch_rate[boat_days] <- chinook_fish_rate
 sockeye_catch_rate <- oneDzeroes
+sockeye_catch_rate[boat_days] <- sockeye_fish_rate
+
+# v_smolts <- oneDzeroes
+# v_smolts[91:244] <- v
+# as a secondary adjustment to C
+# April 1 - Sept 1 right now
 
 harvest_plan <- createHarvestPlan(scenario = "Boat", days = days, years = years, 
                                   boat_days = boat_days, salmon_days = salmon_days)
@@ -49,4 +61,4 @@ eaten_sockeye <- oneDzeroes
 eaten_coho <- oneDzeroes
 consumed_total <- oneDzeroes
 
-kill_list <- c()
+
--- Functions/set_pars.R
+++ Functions/set_pars.R
@@ -1,19 +1,22 @@
 ## Set Up Parameters ----
 
+library(lubridate)
+
 # loop parameters
 days <- 365
 
 # seal parameters
-num_seals <- 10
+num_seals <- 150
 
 # seal consumption parameters
-alpha <- 0.1
-Cmax <- 5
+alpha <- 0.1 
+Cmax <- 1 # this was initially made up but actually makes some sense
 gamma <- 0
-Y <- 0
+Y <- 0 # this freaks out when I make it > 0
 
 # seal learning parameters
-w <- 1
+w <- 0.2
+# v <- 0.1
 ymin <- -10
 ymax <- 0
 xmin <- -1
@@ -23,11 +26,11 @@ threshold <- -5
 slope_x <- 0.1
 intercept_x <- 0.1
 step <- 0.25
-decay <- 0.1
+decay <- 0.05
 buffer_Pymin <- 0
 
 # seal social learning parameters
-num_seals_2_copy <- 3
+num_seals_2_copy <- 2
 mean <- 0.5 # of the beta dist
 beta <- 15 # spread of the beta dist
 
@@ -39,10 +42,16 @@ natural_mort <- 0.0005
 
 # hunting parameters
 zone_efficiency <- 0.8
-steepness_H <- 10 # how quick does it saturate
-efficiency <- 0.2 # what prop of seals are they capable of taking
+steepness_H <- 10 # how quick does it saturate (higher = slower)
+efficiency <- 0.05 # what prop of seals are they capable of taking
 min_fishers <- 13
 max_fishers <- 25
-coho_fish_rate <- 0.1
+fishery_open <- yday(as.Date("2023-09-10"))
+fishery_close <- yday(as.Date("2023-11-17"))
+boat_days <- fishery_open:fishery_close
+
+coho_fish_rate <- 0.1 # see "estFishingRate.R"
+chinook_fish_rate <- 0.01
+sockeye_fish_rate <- 0.01
 salmon_days <- which(Daily_fish$total > 0)
 
