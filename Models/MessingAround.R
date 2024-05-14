97a7df5c4f7eab0b649f667fcf4ac1a56512e098
Liz Allyn <liz.allyn@allyn.org>
2024-05-14 16:58:27 -0700

    exploring parameter space more

--- Models/MessingAround.R
+++ Models/MessingAround.R
@@ -10,8 +10,10 @@ source("Functions/createHarvestPlan.R")
 source("Functions/BaseRun_set_pars.R")
 
 # make changes
-num_seals <- 150
+num_seals <- 20
 intercept_x <- 0.01
+num_seals_2_copy <- num_seals/2
+w <- 0.5
 
 source("Functions/BaseRun_initialize_variables.R")
 
