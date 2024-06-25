c5d3844ca10672b0cf1512f837cda742e589ab28
Liz Allyn <liz.allyn@allyn.org>
2024-06-24 17:02:45 -0700

    nothing done yet but computer flickered. Trying to fix so salmon actually update daily...

--- Functions/salmonSpeciesUpdate.R
+++ Functions/salmonSpeciesUpdate.R
@@ -1,12 +1,13 @@
 # function that returns the arriving salmon of each species for a given day of the year
+# !!!!If you add steelhead data make sure to fix the data stream reference below!!!!
 
 salmonSpeciesUpdate <- function(day, sockeye = 0, chinook = 0, coho = 0, chum = 0, pink = 0, steelhead = 0, data) {
-  Chinook_day <- chinook + (data %>% slice(day) %>% pull(AvgChinook))
-  Sockeye_day <- sockeye + (data %>% slice(day) %>% pull(AvgSockeye))
-  Coho_day <- coho + (data %>% slice(day) %>% pull(AvgCoho))
-  Chum_day <- chum + (data %>% slice(day) %>% pull(AvgChum))
-  Pink_day <- pink + (data %>% slice(day) %>% pull(AvgPink))
-  Steelhead_day <- pink + (data %>% slice(day) %>% pull(AvgSteelhead))
+  Chinook_day <- chinook + (data %>% slice(day) %>% pull(AvgChinook)) - (data %>% slice(day-1) %>% pull(AvgChinook))
+  Sockeye_day <- sockeye + (data %>% slice(day) %>% pull(AvgSockeye)) - (data %>% slice(day-1) %>% pull(AvgSockeye))
+  Coho_day <- coho + (data %>% slice(day) %>% pull(AvgCoho)) - (data %>% slice(day-1) %>% pull(AvgCoho))
+  Chum_day <- chum + (data %>% slice(day) %>% pull(AvgChum)) - (data %>% slice(day-1) %>% pull(AvgChum))
+  Pink_day <- pink + (data %>% slice(day) %>% pull(AvgPink)) - (data %>% slice(day-1) %>% pull(AvgPink))
+  Steelhead_day <- steelhead + (data %>% slice(day) %>% pull(AvgSteelhead)) - (data %>% slice(day-1) %>% pull(AvgSteelhead))
   
  return(data.frame(Chinook = Chinook_day,
              Sockeye = Sockeye_day,
@@ -16,4 +17,4 @@ salmonSpeciesUpdate <- function(day, sockeye = 0, chinook = 0, coho = 0, chum = 
              Steelhead = Steelhead_day, row.names = c("count"))
  )
 }
-
+salmonSpeciesUpdate(12, sockeye = 15, data = Chosen_fish)
