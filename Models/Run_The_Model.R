e01c47b107a1373306c188a3afcc12a55569c2de
Liz Allyn <liz.allyn@allyn.org>
2024-06-25 14:16:30 -0700

    working right now, missed a couple changes on earlier ones that are fixed now

--- Models/Run_The_Model.R
+++ Models/Run_The_Model.R
@@ -12,13 +12,13 @@ assembleTheLegos(path_to_pars = "Functions/BaseRun_set_pars.R",
 # salmon_escapement
 # 
 # 
-# plot_Px + plot_probs + plot_seals + plot_layout(guides = "collect")
-# 
-# plot_seals/plot_H/plot_y/plot_Py + plot_layout(guides = "collect")
-# 
-# eaten_sp_plot/plot_eaten/plot_C/plot_x/plot_Px + plot_layout(guides = "collect")
-# 
-# gauntlet_plot / eaten_sp_plot / fished_plot / escape_plot + plot_layout(guides = "collect")
+plot_Px + plot_probs + plot_seals + plot_layout(guides = "collect")
+
+plot_seals/plot_H/plot_y/plot_Py + plot_layout(guides = "collect")
+
+eaten_sp_plot/plot_eaten/plot_C/plot_x/plot_Px + plot_layout(guides = "collect")
+
+gauntlet_plot / eaten_sp_plot / fished_plot / escape_plot + plot_layout(guides = "collect")
 # 
 # 
 # x_over_Px_plot / y_over_Py_plot
