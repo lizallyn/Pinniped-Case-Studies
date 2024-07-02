2af959ff27cc0b1a123c7c37ff51fa2ff169af4c
Liz Allyn <liz.allyn@allyn.org>
2024-07-01 17:01:56 -0700

    uncomment

--- Models/Run_The_Model.R
+++ Models/Run_The_Model.R
@@ -1,35 +1,36 @@
 source("Functions/assembleTheLegos.R")
-# assembleTheLegos(path_to_pars = "Functions/BaseRun_set_pars.R", 
-#                  path_to_vars = "Functions/BaseRun_initialize_variables.R")
+assembleTheLegos(path_to_pars = "Functions/BaseRun_set_pars.R",
+                 path_to_vars = "Functions/BaseRun_initialize_variables.R")
 
-# assembleFirstHalf(path_to_pars = "Markdown Outputs/ZoneHigh_set_pars.R")
-# num_seals <- 2
-# assembleSecondHalf(path_to_vars = "Functions/BaseRun_initialize_variables.R")
-# salmon_escapement
+assembleFirstHalf(path_to_pars = "Markdown Outputs/ZoneHigh_set_pars.R")
 
-# num_seals <- 50
-# efficiency <- 0.5
-# harvest_close <- end_loop - 2
-# harvest_open <- start_loop + 2
-# min_harvesters <- 20
-# max_harvesters <- 30
-# assembleSecondHalf(path_to_vars = "Functions/BaseRun_initialize_variables.R")
-# salmon_escapement
-# 
-# 
-# plot_Px + plot_probs + plot_seals + plot_layout(guides = "collect")
-# 
-# plot_seals/plot_H/plot_y/plot_Py + plot_layout(guides = "collect")
-# 
-# eaten_sp_plot/plot_eaten/plot_C/plot_x/plot_Px + plot_layout(guides = "collect")
-# 
-# gauntlet_plot / eaten_sp_plot / fished_plot / escape_plot + plot_layout(guides = "collect")
+num_seals <- 2
+assembleSecondHalf(path_to_vars = "Functions/BaseRun_initialize_variables.R")
+salmon_escapement
+
+num_seals <- 50
+efficiency <- 0.5
+harvest_close <- end_loop - 2
+harvest_open <- start_loop + 2
+min_harvesters <- 20
+max_harvesters <- 30
+assembleSecondHalf(path_to_vars = "Functions/BaseRun_initialize_variables.R")
+salmon_escapement
 
 
-# 
-# 
-# x_over_Px_plot / y_over_Py_plot
-# 
+plot_Px + plot_probs + plot_seals + plot_layout(guides = "collect")
+
+plot_seals/plot_H/plot_y/plot_Py + plot_layout(guides = "collect")
+
+eaten_sp_plot/plot_eaten/plot_C/plot_x/plot_Px + plot_layout(guides = "collect")
+
+gauntlet_plot / eaten_sp_plot / fished_plot / escape_plot + plot_layout(guides = "collect")
+
+
+
+
+x_over_Px_plot / y_over_Py_plot
+#
 # 
 # 
 # 
