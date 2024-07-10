888cc7acf166b944913e846f42d667d7c1f500fc
lizallyn <lizallyn@uw.edu>
2024-07-10 11:18:24 -0700

    roadtrip work

--- Functions/WithSL_Plots.R
+++ Functions/WithSL_Plots.R
@@ -1,12 +1,12 @@
 ## Plots for Visualizing model results
 
-# with ggplot to show each seal individually
-
+# Packages ----
 library(reshape2)
 library(ggplot2)
 library(patchwork)
 
-# plot_x_range <- start_loop:end_loop
+
+# Create Functions ----
 
 prepForPlots <- function(df, key.col = "Seal", 
                          other.cols = "Day", value.col){
@@ -62,7 +62,9 @@ makePlot_3 <- function(x, data, col.names, variable.name, value.name, colors){
 }
 
 
-# Plots of Individual Seals
+
+# Plots of Individual Seals ----
+
 plot_probs <- makePlot_1(seal_prob_gauntlet, "Gauntlet Probabilities", colors)
 plot_eaten <- makePlot_1(salmon_consumed_pv, "Salmon Eaten per Seal", colors)
 plot_C <- makePlot_1(C, "C (adjusted consumption)", colors)
@@ -72,14 +74,14 @@ plot_y <- makePlot_1(y, "y (harvest risk opinion", colors)
 plot_Py <- makePlot_1(P_y, "P_y", colors)
 plot_Psoc <- makePlot_1(P_social, "P_social", colors)
 
-# Plots of Aggregated 1D data
+# Plots of Aggregated 1D data ----
 plot_seals <- makePlot_2(x = 1:days, x.name = "Day", y = colSums(seal_forage_loc), 
                          y.name = "Num Seals at Gauntlet", color = "turquoise3")
 plot_consumed <- makePlot_2(x = 1:days, x.name = "Day", y = consumed_total, y.name = "Daily Salmon Consumed", 
                             color = "dodgerblue")
 plot_H <- makePlot_2(x = 1:days, x.name = "Day", y = H, y.name = "Seals Harvested", color = "orchid")
 
-# Plots of Salmon Species data
+# Plots of Salmon Species data ----
 
 escape_plot <- makePlot_3(x = 1:days, data = cbind(escape_chinook, escape_sockeye, escape_coho),
            col.names = c("Day", "Chinook", "Sockeye", "Coho"), variable.name = "Species", 
@@ -95,7 +97,7 @@ fished_plot <- makePlot_3(x = 1:days, data = cbind(fished_chinook, fished_sockey
                           value.name = "Daily Fished Salmon", colors = salmon.colors)
 
 
-# OTHER
+# OTHER ----
 
 list_y <- seq(ymin, ymax, 0.1)
 y_over_Py_plot <- ggplot() + 
