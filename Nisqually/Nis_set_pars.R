7b560267fedf4bebde17ffbf1830d1907d281d7b
Liz Allyn <liz.allyn@allyn.org>
2024-08-06 10:50:07 -0700

    using average run size for the moment

--- Nisqually/Nis_set_pars.R
+++ Nisqually/Nis_set_pars.R
@@ -6,22 +6,26 @@ dates_buffer <- 20
 
 ## Salmon ----
 
+# run.size is a distribution based on the description Craig sent, 
+  # so we can draw a random run size if wanted.
+# going to just use average for now though to make troubleshooting easier.
+
 # Chum
 chum.run.min <- 2000
 chum.run.max <- 62000
 chum.run.avg <- 27000
-chum.run.size <- rbeta(n = 1, 4, 5) * chum.run.max
+# chum.run.size <- rbeta(n = 1, 4, 5) * chum.run.max
 
 # Green river Chinook
 gr.run.min <- 6000
 gr.run.max <- 42000
 gr.run.avg <- 22000
-gr.run.size <- rnorm(1, gr.run.avg, 5500)
+# gr.run.size <- rnorm(1, gr.run.avg, 5500)
 
 # LocNis Chinook
 ln.run.min <- 300
 ln.run.max <- 1400
 ln.run.avg <- 630
-ln.run.size <- (rbeta(1, 3.5, 6)+0.1) * ln.run.max
+# ln.run.size <- (rbeta(1, 3.5, 6)+0.1) * ln.run.max
 
 
--- Nisqually/Prep_Nisqually_Salmon_Data.R
+++ Nisqually/Prep_Nisqually_Salmon_Data.R
@@ -3,6 +3,8 @@ library(tidyr)
 library(dplyr)
 library(lubridate)
 
+source("~/GitHub/PinnipedCaseStudies/Nisqually/predictFish.R")
+
 fish.wide <- read.csv("Data/Nisqually/Nisqually_Chinook_and_Chum_July2024.csv")
 
 ## CHUM ----
@@ -10,21 +12,22 @@ chum_start <- min(which(fish.wide$Chum_per > 0)) - dates_buffer
 chum_end <- max(which(fish.wide$Chum_per > 0)) + dates_buffer
 
 Chum <- data.frame(fish.wide[chum_start:chum_end, c("Date", "DayofYear", "Chum_per")])
-Chum$DailyEst <- Chum$Chum_per * chum.run.size
+Chum$DailyEst <- Chum$Chum_per * chum.run.avg
 Chum$DailyEst_int <- floor(Chum$DailyEst)
 
 # plot(Chum$DayofYear, Chum$DailyEst)
 #looks kinda normal enough for a rough estimate I think
 
-params <- c(90196.94, 68.76944, 14.56296)
+params <- c(194999, 68.769, 14.57)
 fish.fit.optim.chum <- fish.fit.optim(params, fit.to.fish, Chum$DailyEst_int)
+fish.fit.optim.chum
 chum_params <- fish.fit.optim.chum$par
 
-# plot(Chum$DayofYear, Chum$DailyEst)
-# lines(chum_start:chum_end, predictFish(chum_params, day = chum_start:chum_end, start.day = chum_start))
+plot(Chum$DayofYear, Chum$DailyEst)
+lines(chum_start:chum_end, predictFish(chum_params, day = chum_start:chum_end, start.day = chum_start))
 # looks ok to me!
 
-# plot(chum_start:chum_end, predictNewFish(chum_params, day = chum_start:chum_end, chum_start))
+plot(chum_start:chum_end, predictNewFish(chum_params, day = chum_start:chum_end, chum_start))
 
 
 ## GREEN RIVER CHINOOK ----
@@ -34,18 +37,18 @@ gr_end <- max(which(fish.wide$GreenRiver_per > 0)) + dates_buffer
 
 GR_Chinook <- data.frame(fish.wide[gr_start:gr_end, 
                                 c("Date", "DayofYear", "GreenRiver_per")])
-GR_Chinook$DailyEst <- GR_Chinook$GreenRiver_per * gr.run.size
-GR_Chinook$DailyEst_int <- floor(GR_Chinook$GreenRiver_per * gr.run.size)
+GR_Chinook$DailyEst <- GR_Chinook$GreenRiver_per * gr.run.avg
+GR_Chinook$DailyEst_int <- floor(GR_Chinook$DailyEst)
 
-params <- c(178398.40890, 73.54254, 15.21292)
+params <- c(173431.97006, 73.53288, 15.18786)
 fish.fit.optim.gr <- fish.fit.optim(params, fit.to.fish, GR_Chinook$DailyEst_int)
 gr_params <- fish.fit.optim.gr$par
 
-# plot(GR_Chinook$DayofYear, GR_Chinook$DailyEst)
-# lines(gr_start:gr_end, predictFish(gr_params, day = gr_start:gr_end, start.day = gr_start))
-# # looks good!
-# 
-# plot(gr_start:gr_end, predictNewFish(gr_params, day = gr_start:gr_end, start.day = gr_start))
+plot(GR_Chinook$DayofYear, GR_Chinook$DailyEst)
+lines(gr_start:gr_end, predictFish(gr_params, day = gr_start:gr_end, start.day = gr_start))
+# looks good!
+
+plot(gr_start:gr_end, predictNewFish(gr_params, day = gr_start:gr_end, start.day = gr_start))
 
 
 ## LocNis CHINOOK ----
@@ -55,17 +58,17 @@ locnis_end <- max(which(fish.wide$LocNis_per_corr > 0)) + dates_buffer
 
 LocNis_Chinook <- data.frame(fish.wide[locnis_start:locnis_end, 
                                        c("Date", "DayofYear", "LocNis_per_corr")])
-LocNis_Chinook$DailyEst <- LocNis_Chinook$LocNis_per_corr * ln.run.size
+LocNis_Chinook$DailyEst <- (LocNis_Chinook$LocNis_per_corr * ln.run.avg)/7
 LocNis_Chinook$DailyEst_int <- floor(LocNis_Chinook$DailyEst)
 
-params <- c(5269.54482, 84.98617, 26.72271)
+params <- c(590, 85, 25)
 fish.fit.optim.ln <- fish.fit.optim(params, fit.to.fish, LocNis_Chinook$DailyEst_int)
 fish.fit.optim.ln
 ln_params <- fish.fit.optim.ln$par
 
-# plot(LocNis_Chinook$DayofYear, LocNis_Chinook$DailyEst)
-# lines(locnis_start:locnis_end, predictFish(ln_params, day = locnis_start:locnis_end, start.day = locnis_start))
-# # ehh looks not great but maybe workable for now.
-# 
-# plot(locnis_start:locnis_end, predictNewFish(ln_params, day = locnis_start:locnis_end, start.day = locnis_start))
+plot(LocNis_Chinook$DayofYear, LocNis_Chinook$DailyEst)
+lines(locnis_start:locnis_end, predictFish(ln_params, day = locnis_start:locnis_end, start.day = locnis_start))
+# ehh looks not great but maybe workable for now.
+
+plot(locnis_start:locnis_end, predictNewFish(ln_params, day = locnis_start:locnis_end, start.day = locnis_start))
 
