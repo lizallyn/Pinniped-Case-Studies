24c4047063081a6fc96bcd2e2888a9678d6dbec0
Liz Allyn <liz.allyn@allyn.org>
2024-08-06 15:46:52 -0700

    more updates for chum

--- Nisqually/000_Run_The_Model_Nis_Chum.R
+++ Nisqually/000_Run_The_Model_Nis_Chum.R
@@ -10,14 +10,14 @@ source("Functions/makeArray.R")
 source("Functions/createHarvestPlan.R")
 
 # 01 Set Parameters
-source("Nisqually/01_Nis_set_pars.R")
+source("Nisqually/01_Nis_set_pars_Chum.R")
 
 # 02 Prep Data
 source("Nisqually/02_Prep_Nisqually_Salmon_Data.R")
 
 # 03 Initialize Variables
 source("Nisqually/03_Nis_set_pars_Chum.R")
-
+source("Nisqually/03_Nis_initialize_variables_Chum.R")
 
 # 04 Loop Functions
 source("Nisqually/04_salmonSpeciesUpdate_Chum.R")
@@ -33,3 +33,6 @@ source("Functions/updateLearning.R")
 
 # 05 Run The Loop
 source("~/GitHub/PinnipedCaseStudies/Nisqually/05_The_Loop_Nis_Chum.R")
+
+# 06 Plots
+
