216ff546b2e4dfbfee8c204a909990f6930ad987
Liz Allyn <liz.allyn@allyn.org>
2024-08-07 14:35:02 -0700

    fix weird salmon numbers

--- Nisqually/02_Prep_Nisqually_Salmon_Data.R
+++ Nisqually/02_Prep_Nisqually_Salmon_Data.R
@@ -12,19 +12,20 @@ chum_start <- min(which(fish.wide$Chum_per > 0)) - dates_buffer
 chum_end <- max(which(fish.wide$Chum_per > 0)) + dates_buffer
 chum_residence <- 21
 
-Chum <- data.frame(fish.wide[chum_start:chum_end, c("Date", "DayofYear", "Chum_per")])
-Chum$DailyEst <- Chum$Chum_per * chum.run.avg
-Chum$DailyEst_int <- floor(Chum$DailyEst)
+WChum <- data.frame(fish.wide[chum_start:chum_end, c("Date", "DayofYear", "Chum_per")])
+WChum$DailyEst <- (WChum$Chum_per * chum.run.avg)/7
+WChum$DailyEst_int <- round(WChum$DailyEst)
 
 # plot(Chum$DayofYear, Chum$DailyEst)
 #looks kinda normal enough for a rough estimate I think
 
-params <- c(194999, 68.769, 14.57)
-fish.fit.optim.chum <- fish.fit.optim(params, fit.to.fish, Chum$DailyEst_int)
+params <- c(27800, 68.769, 14.59)
+fish.fit.optim.chum <- fish.fit.optim(params, fit.to.fish, WChum$DailyEst_int)
 fish.fit.optim.chum
 chum_params <- fish.fit.optim.chum$par
 
-# plot(Chum$DayofYear, Chum$DailyEst)
+# check fit!
+# plot(WChum$DayofYear, WChum$DailyEst)
 # lines(chum_start:chum_end, predictFish(chum_params, day = chum_start:chum_end, start.day = chum_start))
 # # looks ok to me!
 # 
@@ -40,16 +41,18 @@ gr_end <- max(which(fish.wide$GreenRiver_per > 0)) + dates_buffer
 
 gr_residence <- 14
 
-GR_Chinook <- data.frame(fish.wide[gr_start:gr_end, 
+GRiver_Chinook <- data.frame(fish.wide[gr_start:gr_end, 
                                 c("Date", "DayofYear", "GreenRiver_per")])
-GR_Chinook$DailyEst <- GR_Chinook$GreenRiver_per * gr.run.avg
-GR_Chinook$DailyEst_int <- floor(GR_Chinook$DailyEst)
+GRiver_Chinook$DailyEst <- (GRiver_Chinook$GreenRiver_per * gr.run.avg)/7
+GRiver_Chinook$DailyEst_int <- round(GRiver_Chinook$DailyEst)
 
-params <- c(173431.97006, 73.53288, 15.18786)
-fish.fit.optim.gr <- fish.fit.optim(params, fit.to.fish, GR_Chinook$DailyEst_int)
+params <- c(22000, 73, 15)
+fish.fit.optim.gr <- fish.fit.optim(params, fit.to.fish, GRiver_Chinook$DailyEst_int)
+fish.fit.optim.gr
 gr_params <- fish.fit.optim.gr$par
 
-# plot(GR_Chinook$DayofYear, GR_Chinook$DailyEst)
+# check fit!
+# plot(GRiver_Chinook$DayofYear, GRiver_Chinook$DailyEst)
 # lines(gr_start:gr_end, predictFish(gr_params, day = gr_start:gr_end, start.day = gr_start))
 # # looks good!
 # 
@@ -65,19 +68,19 @@ locnis_residence <- 7
 
 LocNis_Chinook <- data.frame(fish.wide[locnis_start:locnis_end, 
                                        c("Date", "DayofYear", "LocNis_per_corr")])
-LocNis_Chinook$DailyEst <- (LocNis_Chinook$LocNis_per_corr * ln.run.avg)
-LocNis_Chinook$DailyEst_int <- floor(LocNis_Chinook$DailyEst)
+LocNis_Chinook$DailyEst <- (LocNis_Chinook$LocNis_per_corr * ln.run.avg)/7
+LocNis_Chinook$DailyEst_int <- round(LocNis_Chinook$DailyEst)
 
-params <- c(5104.17195, 84.91841, 26.77771)
+params <- c(645.8, 86, 27)
 fish.fit.optim.ln <- fish.fit.optim(params, fit.to.fish, LocNis_Chinook$DailyEst_int)
 fish.fit.optim.ln
 ln_params <- fish.fit.optim.ln$par
 
-# plot(LocNis_Chinook$DayofYear, LocNis_Chinook$DailyEst)
-# lines(locnis_start:locnis_end, predictFish(ln_params, day = locnis_start:locnis_end, start.day = locnis_start))
-# # ehh looks not great but maybe workable for now.
-# 
-# plot(locnis_start:locnis_end, predictNewFish(ln_params, day = locnis_start:locnis_end, start.day = locnis_start))
+plot(LocNis_Chinook$DayofYear, LocNis_Chinook$DailyEst)
+lines(locnis_start:locnis_end, predictFish(ln_params, day = locnis_start:locnis_end, start.day = locnis_start))
+# ehh looks not great but maybe workable for now.
+
+plot(locnis_start:locnis_end, predictNewFish(ln_params, day = locnis_start:locnis_end, start.day = locnis_start))
 
 chinook_start <- min(locnis_start, gr_start)
 chinook_end <- max(locnis_end, gr_end)
@@ -94,7 +97,7 @@ fish_end <- max(chum_end, chinook_end)
 fish_days <- fish_start:fish_end
 
 Daily_Fish <- data.frame(DayofYear = fish_days,
-                         Chum = round(predictNewFish(chum_params, day = fish_days, chum_start)),
-                         GR_Chinook = round(predictNewFish(gr_params, day = fish_days, start.day = gr_start)),
-                         LN_Chinook = round(predictNewFish(ln_params, day = fish_days, start.day = locnis_start)))
+                         Chum = round(predictFish(chum_params, day = fish_days, chum_start)),
+                         GR_Chinook = round(predictFish(gr_params, day = fish_days, start.day = gr_start)),
+                         LN_Chinook = round(predictFish(ln_params, day = fish_days, start.day = locnis_start)))
 Daily_Fish$Total <- Daily_Fish$Chum + Daily_Fish$GR_Chinook + Daily_Fish$LN_Chinook
