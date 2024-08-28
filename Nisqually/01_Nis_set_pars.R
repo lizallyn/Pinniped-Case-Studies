c5affb06f8cda8d3c8834d687b6d8be7132cab46
Liz Allyn <liz.allyn@allyn.org>
2024-08-28 14:56:10 -0700

    updated run sizes from Craig

--- Nisqually/01_Nis_set_pars.R
+++ Nisqually/01_Nis_set_pars.R
@@ -7,25 +7,25 @@ dates_buffer <- 20
 ## Salmon ----
 
 # run.size is a distribution based on the description Craig sent, 
-  # so we can draw a random run size if wanted.
+  # so we can draw a random run size if wanted. This was not updated.
 # going to just use average for now though to make troubleshooting easier.
 
 # Chum
 chum.run.min <- 2000
 chum.run.max <- 62000
-chum.run.avg <- 27000
+chum.run.avg <- 25912
 # chum.run.size <- rbeta(n = 1, 4, 5) * chum.run.max
 
 # Green river Chinook
 gr.run.min <- 6000
 gr.run.max <- 42000
-gr.run.avg <- 22000
+gr.run.avg <- 18445
 # gr.run.size <- rnorm(1, gr.run.avg, 5500)
 
 # LocNis Chinook
 ln.run.min <- 300
 ln.run.max <- 1400
-ln.run.avg <- 630
+ln.run.avg <- 779
 # ln.run.size <- (rbeta(1, 3.5, 6)+0.1) * ln.run.max
 
 
