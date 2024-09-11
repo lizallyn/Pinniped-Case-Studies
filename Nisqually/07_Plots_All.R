85115cd9ceaa484a32cf5170aed839c492344db0
Liz Allyn <liz.allyn@allyn.org>
2024-09-11 13:23:47 -0700

    added plots for raw arrival data showing the normal curve fits and a plot for daily catch rate derived from the model base run

--- Nisqually/07_Plots_All.R
+++ Nisqually/07_Plots_All.R
@@ -22,9 +22,15 @@ salmon.colors <- c("dodgerblue", "seagreen2", "seagreen4")
 salmon.names <- c("Chum", "Chinook_GR", "Chinook_LN")
 names(salmon.colors) <- salmon.names
 
+
 arrival_plot <- makePlot_3(x = 1:days, data = Daily_Fish[,c("Chum", "GR_Chinook", "LN_Chinook")], 
                            col.names = c("Day", "Chum", "Chinook_GR", "Chinook_LN"), variable.name = "Species", 
-                           value.name = "Daily Arriving Salmon", colors = salmon.colors)
+                           value.name = "Daily Arriving Salmon (Fitted)", colors = salmon.colors)
+raw.arrival <- makePlot_3(x = 1:days, data = fish.wide[1:days, c("Chum", "GR", "LocNis")],
+                          col.names = c("Day", "Chum", "Chinook_GR", "Chinook_LN"), variable.name = "Species",
+                          value.name = "Daily Arriving Salmon (Data)", colors = salmon.colors)
+raw_arrival_plot <- arrival_plot + 
+  geom_point(aes(x = raw.arrival$data[,1], y = raw.arrival$data[,3], color = raw.arrival$data[,2]), shape = 1)
 escape_plot <- makePlot_3(x = 1:days, data = cbind(escape_chum, escape_gr, escape_ln),
                           col.names = c("Day", "Chum", "Chinook_GR", "Chinook_LN"), variable.name = "Species", 
                           value.name = "Cumulative Escaped Salmon", colors = salmon.colors)
@@ -37,3 +43,6 @@ gauntlet_plot <- makePlot_3(x = 1:days, data = cbind(gauntlet_chum, gauntlet_gr,
 fished_plot <- makePlot_3(x = 1:days, data = cbind(fished_chum, fished_gr, fished_ln), 
                           col.names = c("Day", "Chum", "Chinook_GR", "Chinook_LN"), variable.name = "Species",
                           value.name = "Daily Salmon Fished", colors = salmon.colors)
+catch_rate_plot <- makePlot_3(x = 1:days, data = cbind(chum_catch_rate, gr_catch_rate, ln_catch_rate), 
+                          col.names = c("Day", "Chum", "Chinook_GR", "Chinook_LN"), variable.name = "Species",
+                          value.name = "Daily Salmon Catch Rate", colors = salmon.colors)
