e2ec9fc7a9f3274794f181a5120e37d45f1666a1
Liz Allyn <liz.allyn@allyn.org>
2024-09-30 11:11:38 -0700

    new folder for ballard runs

---
+++ Ballard/00_Run_The_Model_Ballard.R
@@ -0,0 +1,11 @@
+# Run the model for all three pinnipeds and salmon runs
+
+# clear environment
+rm(list=ls())
+# setwd("/Users/lizallyn/Documents/GitHub/Pinniped-Case-Studies")
+
+# 01 Set-Up Functions
+source("Functions/01_predictFish.R")
+source("Functions/makeArray.R")
+source("Functions/createHarvestPlan.R")
+
