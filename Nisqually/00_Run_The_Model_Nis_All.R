76ab964440c8abf9cf68c31d5473a7aea34d40f4
Liz Allyn <liz.allyn@allyn.org>
2024-09-30 11:11:53 -0700

    call predictfish from new folder loc

--- Nisqually/00_Run_The_Model_Nis_All.R
+++ Nisqually/00_Run_The_Model_Nis_All.R
@@ -7,7 +7,7 @@ rm(list=ls())
 # setwd("/Users/lizallyn/Documents/GitHub/Pinniped-Case-Studies")
 
 # 01 Set-Up Functions
-source("Nisqually/01_predictFish.R")
+source("Functions/01_predictFish.R")
 source("Functions/makeArray.R")
 source("Functions/createHarvestPlan.R")
 
