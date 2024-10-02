9a97f8ef45eedcdb93ba66394279179b74f126c9
Liz Allyn <liz.allyn@allyn.org>
2024-10-02 10:20:49 -0700

    literally did the same calculations again because I didn't remember...idiot

--- Recycle Bin/Functions/estFishingRates.R
+++ Recycle Bin/Functions/estFishingRates.R
@@ -18,3 +18,18 @@ merged$avg.chinook.catch.rate <- merged$ChinookCatch/merged$AvgChinook
 merged$avg.chinook.catch.rate[which(is.infinite(merged$avg.chinook.catch.rate))] <- 0
 merged$avg.chinook.catch.rate[which(is.nan(merged$avg.chinook.catch.rate))] <- 0
 mean(merged$avg.chinook.catch.rate, na.rm = T)
+
+# Doing again Oct 2024 bc I don't remember doing this?
+
+source("Ballard/02_Prep_Ballard_salmon_data.R")
+
+gauntlet_fish <- data.frame(Sockeye = gauntlet_sockeye, Chinook = gauntlet_chinook, 
+                            Coho = gauntlet_coho)
+combo <- cbind(Chosen_catch, gauntlet_fish)
+
+combo$sockeye_rate <- combo$SockeyeCatch/combo$Sockeye
+combo$chinook_rate <- combo$ChinookCatch/combo$Chinook
+combo$coho_rate <- combo$CohoCatch/combo$Coho
+
+mean(combo$coho_rate, na.rm = T)
+mean(combo$chinook_rate, na.rm = T)
