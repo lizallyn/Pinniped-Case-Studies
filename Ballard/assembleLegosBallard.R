0e77eb5815ee6f5a136f7acc970bcfef04ff6d3e
Liz Allyn <liz.allyn@allyn.org>
2024-10-03 16:54:17 -0700

    for running the plots mostly with fewer lines int he manipulation slides code

---
+++ Ballard/assembleLegosBallard.R
@@ -0,0 +1,11 @@
+
+source("Ballard/05_salmonSpeciesUpdate.R")
+
+source("Ballard/06_The_Loop.R")
+
+source("Functions/makePlots.R")
+source("Functions/Plots_Pv.R")
+source("Functions/Plots_Ej.R")
+source("Functions/Plots_Zc.R")
+source("Functions/Plots_salmon.R")
+source("Functions/Plots_responses.R")
