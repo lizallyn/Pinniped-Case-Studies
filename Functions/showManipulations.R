cac978bcc189b0010e9c57cc0108facbe5c7c3aa
Liz Allyn <liz.allyn@allyn.org>
2024-10-10 16:01:39 -0700

    switch colors to make more sense.

--- Functions/showManipulations.R
+++ Functions/showManipulations.R
@@ -4,9 +4,9 @@ showManipulations <- function(output, base_val) {
   output <- data.frame(output)
   
   plot_escape_manipulations <- ggplot(data = output, aes(x = output[,1])) +
-    geom_line(aes(y = Chum), color = "seagreen", lwd = 1.2) + 
-    geom_line(aes(y = Chinook_GR), color = "goldenrod", lwd = 1.2) + 
-    geom_line(aes(y = Chinook_LN), color = "salmon", lwd = 1.2) +
+    geom_line(aes(y = Chum), color = "salmon", lwd = 1.2) + 
+    geom_line(aes(y = Chinook_GR), color = "seagreen", lwd = 1.2) + 
+    geom_line(aes(y = Chinook_LN), color = "goldenrod", lwd = 1.2) +
     geom_vline(xintercept = base_val, lwd = 2, color = "gray", linetype = 3) +
     labs(y = "Salmon Escaped", x = "Parameter Value") +
     theme_classic()
