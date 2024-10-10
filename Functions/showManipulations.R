42342535fafb21819d4dae1d7f35079793bdbc47
Liz Allyn <liz.allyn@allyn.org>
2024-10-10 10:33:41 -0700

    some cleanup

--- Functions/showManipulations.R
+++ Functions/showManipulations.R
@@ -1,18 +1,18 @@
 # to shorten manipulation code
 
-showManipulations <- function(output, parameter, base_val) {
+showManipulations <- function(output, base_val) {
   output <- data.frame(output)
   
-  plot_escape_manipulations <- ggplot(data = output, aes(x = Seals)) +
+  plot_escape_manipulations <- ggplot(data = output, aes(x = output[,1])) +
     geom_line(aes(y = Chum), color = "seagreen", lwd = 1.2) + 
     geom_line(aes(y = Chinook_GR), color = "goldenrod", lwd = 1.2) + 
     geom_line(aes(y = Chinook_LN), color = "salmon", lwd = 1.2) +
-    geom_vline(xintercept = 50, lwd = 2, color = "gray", linetype = 3) +
+    geom_vline(xintercept = base_val, lwd = 2, color = "gray", linetype = 3) +
     labs(y = "Salmon Escaped", x = "Parameter Value") +
     theme_classic()
   
   escape_4_tab <- rbind(output[1,], output[nrow(output),])
-  escape_4_tab <- escape_4_tab %>% rename_at("Seals", ~ "Run")
+  escape_4_tab <- escape_4_tab %>% rename_at(colnames(escape_4_tab)[1], ~ "Run")
   escapement_table <- rbind(base_salmon_escapement, escape_4_tab)
   
   returnables <- list()
@@ -22,24 +22,5 @@ showManipulations <- function(output, parameter, base_val) {
   return(returnables)
 }
 
-# plot <- showManipulations(Escape, "num_seals", 150)[1]
-# table <- showManipulations(Escape, "num_seals", 150)[2]
-# 
-# 
-# final_table
-# Escape <- data_frame(Escape)
-# 
-# plot_escape_manipulations <- ggplot(data = Escape, aes(x = Seals)) +
-#   geom_line(aes(y = Chum), color = "seagreen", lwd = 1.2) + 
-#   geom_line(aes(y = Chinook_GR), color = "goldenrod", lwd = 1.2) + 
-#   geom_line(aes(y = Chinook_LN), color = "salmon", lwd = 1.2) +
-#   geom_vline(xintercept = 50, lwd = 2, color = "gray", linetype = 3) +
-#   labs(y = "Salmon Escaped", x = "Number of Seals in Source Pool") +
-#   theme_classic()
-# plot_escape_manipulations
-# 
-# escape_4_tab <- rbind(Escape[1,], Escape[nrow(Escape),])
-# escape_4_tab <- escape_4_tab %>% rename_at("Seals", ~ "Run")
-# escapement_table <- rbind(base_salmon_escapement, escape_4_tab)
-# 
-# kable(escapement_table, caption = "Escaped Salmon") %>% kable_styling(full_width = F, font_size = font_size)
+test <- showManipulations(Escape, 150)
+test[[1]]
