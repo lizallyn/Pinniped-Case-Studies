31332bea0571d75c73c42c7357b99ef35f787197
Liz Allyn <liz.allyn@allyn.org>
2024-10-16 16:04:09 -0700

    moved this to Ballard folder since it's system-specific

---
+++ Ballard/07_Plots_salmon.R
@@ -0,0 +1,30 @@
+# salmon plots
+
+salmon_escapement <- data.frame(Sockeye = escape_sockeye[days], Chinook = escape_chinook[days],
+                                Coho = escape_coho[days])
+salmon_catch <- data.frame(Sockeye = sum(fished_sockeye), Chinook = sum(fished_chinook),
+                           Coho = sum(fished_coho))
+salmon_eaten <- data.frame(Sockeye = sum(eaten_sockeye), Chinook = sum(eaten_chinook),
+                           Coho = sum(eaten_coho))
+
+plot_consumed <- makePlot_2(x = 1:days, x.name = "Day", y = consumed_total, y.name = "Daily Salmon Consumed", 
+                            color = "dodgerblue")
+
+# Plots of Salmon Species data
+
+salmon.colors <- c("goldenrod", "salmon3", "lightblue3")
+salmon.names <- c("Chinook", "Sockeye", "Coho")
+names(salmon.colors) <- salmon.names
+
+escape_plot <- makePlot_3(x = 1:days, data = cbind(escape_chinook, escape_sockeye, escape_coho),
+                          col.names = c("Day", "Chinook", "Sockeye", "Coho"), variable.name = "Species", 
+                          value.name = "Cumulative Escaped Salmon", colors = salmon.colors)
+eaten_sp_plot <- makePlot_3(x = 1:days, data = cbind(eaten_chinook, eaten_sockeye, eaten_coho),
+                            col.names = c("Day", "Chinook", "Sockeye", "Coho"), variable.name = "Species", 
+                            value.name = "Daily Salmon Eaten", colors = salmon.colors)
+gauntlet_plot <- makePlot_3(x = 1:days, data = cbind(gauntlet_chinook, gauntlet_sockeye, gauntlet_coho),
+                            col.names = c("Day", "Chinook", "Sockeye", "Coho"), variable.name = "Species", 
+                            value.name = "Daily Salmon at Gauntlet", colors = salmon.colors)
+fished_plot <- makePlot_3(x = 1:days, data = cbind(fished_chinook, fished_sockeye, fished_coho), 
+                          col.names = c("Day", "Chinook", "Sockeye", "Coho"), variable.name = "Species",
+                          value.name = "Daily Salmon Fished", colors = salmon.colors)
