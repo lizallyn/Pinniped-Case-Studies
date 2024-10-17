73f503fa6dcdde20dc434e8e03e8ad9ea8a84468
Liz Allyn <liz.allyn@allyn.org>
2024-10-17 13:51:31 -0700

    small fixes

--- Functions/Plots_Pv.R
+++ Functions/Plots_Pv.R
@@ -3,10 +3,9 @@
 # Plots of Individual Seals
 plot_probs <- makePlot_1(seal_prob_gauntlet, "Gauntlet Probabilities", colors)
 plot_eaten <- makePlot_1(salmon_consumed_pv, "Salmon Eaten per Seal", colors)
-plot_C <- makePlot_1(C, "C (adjusted consumption)", colors)
 plot_x <- makePlot_1(x, "x (foraging opinion)", colors)
 plot_Px <- makePlot_1(P_x, "P_x", colors)
-plot_y <- makePlot_1(y, "y (harvest risk opinion", colors)
+plot_y <- makePlot_1(y, "y (harvest risk opinion)", colors)
 plot_Py <- makePlot_1(P_y, "P_y", colors)
 plot_Psoc <- makePlot_1(P_social, "P_social", colors)
 
